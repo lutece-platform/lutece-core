69728a56ed8aca76e1323ddca584625bb41b0c1a
isabelle-lenain <isabelle-lenain@users.noreply.github.com>
2008-01-09 17:58:26 +0000

    Update Copyright
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@6441 bab10101-e421-0410-a517-8ce0973de3ef

--- src/test/java/fr/paris/lutece/portal/business/role/RoleTest.java
+++ src/test/java/fr/paris/lutece/portal/business/role/RoleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2006, Mairie de Paris
+ * Copyright (c) 2002-2008, Mairie de Paris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
