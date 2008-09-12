c6129944bf7afd7249c07d5047fae73c5566c2d8
pierrelevy <pierrelevy@users.noreply.github.com>
2008-09-12 15:56:30 +0000

    LUTECE-862 : Dashboard feature added for BO homepage
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@9442 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/portal/service/dashboard/DashboardComponentEntry.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2002-2008, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.portal.service.dashboard;
+
+/**
+ *
+ */
+public class DashboardComponentEntry
+{
+
+    private String _strName;
+    private String _strComponentClass;
+    private String _strRight;
+    private int _nZone;
+    private int _nOrder;
+
+    /**
+     * Returns the Name
+     * @return The Name
+     */
+    public String getName()
+    {
+        return _strName;
+    }
+
+    /**
+     * Sets the Name
+     * @param strName The Name
+     */
+    public void setName( String strName )
+    {
+        _strName = strName;
+    }
+
+    /**
+     * Returns the ComponentClass
+     * @return The ComponentClass
+     */
+    public String getComponentClass()
+    {
+        return _strComponentClass;
+    }
+
+    /**
+     * Sets the ComponentClass
+     * @param strComponentClass The ComponentClass
+     */
+    public void setComponentClass( String strComponentClass )
+    {
+        _strComponentClass = strComponentClass;
+    }
+
+    /**
+     * Returns the Right
+     * @return The Right
+     */
+    public String getRight()
+    {
+        return _strRight;
+    }
+
+    /**
+     * Sets the Right
+     * @param strRight The Right
+     */
+    public void setRight( String strRight )
+    {
+        _strRight = strRight;
+    }
+
+    /**
+     * Returns the Zone
+     * @return The Zone
+     */
+    public int getZone()
+    {
+        return _nZone;
+    }
+
+    /**
+     * Sets the Zone
+     * @param nZone The Zone
+     */
+    public void setZone( int nZone )
+    {
+        _nZone = nZone;
+    }
+
+    /**
+     * Returns the Order
+     * @return The Order
+     */
+    public int getOrder()
+    {
+        return _nOrder;
+    }
+
+    /**
+     * Sets the Order
+     * @param nOrder The Order
+     */
+    public void setOrder( int nOrder )
+    {
+        _nOrder = nOrder;
+    }
+}
---
+++ src/java/fr/paris/lutece/portal/service/dashboard/DashboardService.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2002-2008, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.portal.service.dashboard;
+
+import fr.paris.lutece.portal.business.user.AdminUser;
+import fr.paris.lutece.portal.service.plugin.Plugin;
+import fr.paris.lutece.portal.service.util.AppLogService;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Dashboard Service
+ */
+public class DashboardService
+{
+    private static DashboardService _singleton = new DashboardService();
+    private List<DashboardComponent> _listComponents = new ArrayList<DashboardComponent>();
+
+    /**
+     * Private Constructor
+     */
+    private DashboardService()
+    {
+    }
+    
+        /**
+     * Return the unique instance
+     * @return The instance
+     */
+    public static DashboardService getInstance(  )
+    {
+        return _singleton;
+    }
+
+
+    /**
+     * Register a Dashboard Component
+     * @param entry The DashboardComponent entry defined in the plugin's XML file
+     * @param plugin The plugin
+     */
+    public void registerDashboardComponent( DashboardComponentEntry entry, Plugin plugin )
+    {
+        try
+        {
+            DashboardComponent dc = (DashboardComponent) Class.forName( entry.getComponentClass(  ) ).newInstance(  );
+            
+            dc.setName( entry.getName(  ) );
+            dc.setRight( entry.getRight());
+            dc.setZone( entry.getZone() );
+            dc.setOrder( entry.getOrder() );
+            _listComponents.add( dc );
+            AppLogService.info( "New Dashboard Component registered : " + entry.getName(  ) );
+        }
+        catch ( InstantiationException e )
+        {
+            AppLogService.error( "Error registering a DashboardComponent : " + e.getMessage(  ), e );
+        }
+        catch ( IllegalAccessException e )
+        {
+            AppLogService.error( "Error registering a DashboardComponent : " + e.getMessage(  ), e );
+        }
+        catch ( ClassNotFoundException e )
+        {
+            AppLogService.error( "Error registering a DashboardComponent : " + e.getMessage(  ), e );
+        }
+    }
+    
+    /**
+     * Gets Data from all components of the zone
+     * @param user The user
+     * @param nZone The dasboard zone
+     * @return Data of all components of the zone
+     */
+    public String getDashboardData( AdminUser user, int nZone )
+    {
+        StringBuffer sbDashboardData = new StringBuffer();
+        for( DashboardComponent dc : _listComponents )
+        {
+            if( dc.getZone() == nZone && user.checkRight( dc.getRight() ))
+            {
+                sbDashboardData.append( dc.getDashboardData( user )  );
+            }
+        }
+        return sbDashboardData.toString();
+    }
+}
