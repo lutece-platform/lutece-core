517a8fdead5ade338677d63d931c355236e1b599
pierrelevy <pierrelevy@users.noreply.github.com>
2008-09-16 15:43:45 +0000

    LUTECE-862 : New Dashboard component for pages count
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@9497 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/portal/web/SiteDashboardComponent.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2002-2008, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+
+package fr.paris.lutece.portal.web;
+
+import fr.paris.lutece.portal.business.page.PageHome;
+import fr.paris.lutece.portal.business.right.Right;
+import fr.paris.lutece.portal.business.right.RightHome;
+import fr.paris.lutece.portal.business.user.AdminUser;
+import fr.paris.lutece.portal.business.user.AdminUserHome;
+import fr.paris.lutece.portal.service.dashboard.DashboardComponent;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.util.html.HtmlTemplate;
+import java.util.HashMap;
+
+/**
+ * SiteDashboardComponent
+ */
+public class SiteDashboardComponent extends DashboardComponent
+{
+    private static final String TEMPLATE_DASHBOARD = "/admin/site/site_dashboard.html";
+    private static final String MARK_PAGES_COUNT = "pages_count";
+    private static final String MARK_URL = "url";
+    private static final String MARK_ICON = "icon";
+
+    
+    public String getDashboardData( AdminUser user )
+    {
+        Right right = RightHome.findByPrimaryKey( getRight() );
+        HashMap model = new HashMap();
+        model.put( MARK_PAGES_COUNT , PageHome.getAllPages().size() );
+        model.put( MARK_URL, right.getUrl() );
+        model.put( MARK_ICON, right.getIconUrl() );
+        HtmlTemplate t = AppTemplateService.getTemplate( TEMPLATE_DASHBOARD, user.getLocale(), model );
+        return t.getHtml();
+    }
+}
