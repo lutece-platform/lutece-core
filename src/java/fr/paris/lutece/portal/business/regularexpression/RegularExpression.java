d867260c2dc6308abb16846483d1ff5a91492133
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2008-09-16 16:31:39 +0000

    LUTECE-859 : adding a service of regular expression in lutece-core
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@9501 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/portal/business/regularexpression/RegularExpression.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2002-2008, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.portal.business.regularexpression;
+
+
+/**
+ *
+ * RegularExpression
+ *
+ */
+public class RegularExpression
+{
+    private int _nIdExpression;
+    private String _strTitle;
+    private String _strValue;
+    private String _strInformationMessage;
+    private String _strErrorMessage;
+    private String _strValidExemple;
+
+    /**
+     *return the id of the regular expression
+     * @return the id of the regular expression
+     */
+    public int getIdExpression(  )
+    {
+        return _nIdExpression;
+    }
+
+    /**
+     * set the id of the regular expression
+     * @param idExpression the id of the regular expression
+     */
+    public void setIdExpression( int idExpression )
+    {
+        _nIdExpression = idExpression;
+    }
+
+    /**
+     * return the error message
+     * @return the error message
+     */
+    public String getErrorMessage(  )
+    {
+        return _strErrorMessage;
+    }
+
+    /**
+     * set the error message
+     * @param errorMessage the error message
+     */
+    public void setErrorMessage( String errorMessage )
+    {
+        _strErrorMessage = errorMessage;
+    }
+
+    /**
+     *
+     * return a valid exemple which is validate by the regular expression
+     * @return a valid exemple
+     */
+    public String getValidExemple(  )
+    {
+        return _strValidExemple;
+    }
+
+    /**
+     * set a valid exemple which is validate by the regular expression
+     * @param exempleValide a valid exemple
+     */
+    public void setValidExemple( String exempleValide )
+    {
+        _strValidExemple = exempleValide;
+    }
+
+    /**
+     *return the information message
+     * @return the information message
+     */
+    public String getInformationMessage(  )
+    {
+        return _strInformationMessage;
+    }
+
+    /**
+     * set the information message
+     * @param informationMessage the information message
+     */
+    public void setInformationMessage( String informationMessage )
+    {
+        _strInformationMessage = informationMessage;
+    }
+
+    /**
+     * return the title of the regular expression
+     * @return the title of the regular expression
+     */
+    public String getTitle(  )
+    {
+        return _strTitle;
+    }
+
+    /**
+     * set the title of the regular expression
+     * @param title the title of the regular expression
+     */
+    public void setTitle( String title )
+    {
+        _strTitle = title;
+    }
+
+    /**
+     * return the value of the regular expression
+     * @return the value of the regular expression
+     */
+    public String getValue(  )
+    {
+        return _strValue;
+    }
+
+    /**
+     * set the value of the regular expression
+     * @param value the value of the regular expression
+     */
+    public void setValue( String value )
+    {
+        _strValue = value;
+    }
+}
---
+++ src/java/fr/paris/lutece/portal/service/regularexpression/RegularExpressionService.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2002-2008, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.portal.service.regularexpression;
+
+import fr.paris.lutece.portal.business.regularexpression.RegularExpression;
+import fr.paris.lutece.portal.service.spring.SpringContextService;
+
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.CannotLoadBeanClassException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+
+import java.util.List;
+
+/**
+ * 
+ * this class provides services for use regular expression
+ */
+public class RegularExpressionService
+{
+    private static final String SPRING_CONTEXT_NAME = "regularexpression";
+    private static RegularExpressionService _singleton;
+    private boolean _bAvailable = true;
+    private IRegularExpressionService _service;
+
+    private RegularExpressionService(  )
+    {
+        try
+        {
+            _service = (IRegularExpressionService) SpringContextService.getPluginBean( SPRING_CONTEXT_NAME,
+                    "regularExpressionService" );
+            _bAvailable = _service != null;
+        }
+        catch ( BeanDefinitionStoreException e )
+        {
+            _bAvailable = false;
+        }
+        catch ( NoSuchBeanDefinitionException e )
+        {
+            _bAvailable = false;
+        }
+        catch ( CannotLoadBeanClassException e )
+        {
+            _bAvailable = false;
+        }
+    }
+
+    /**
+    * Returns the unique instance of the service
+    * @return The instance of the service
+    */
+    public static RegularExpressionService getInstance(  )
+    {
+        if ( _singleton == null )
+        {
+            _singleton = new RegularExpressionService(  );
+        }
+
+        return _singleton;
+    }
+
+    /**
+    *
+    * @return true if the regular expression service is available
+    */
+    public boolean isAvailable(  )
+    {
+        return _bAvailable;
+    }
+
+    /**
+         * return false if the pattern is invalid
+         * @param strPattern the pattern to test
+         * @return  false if the pattern is invalid
+         */
+    boolean isPatternValide( String strPattern )
+    {
+        return _bAvailable ? _service.isPatternValide( strPattern ) : false;
+    }
+
+    /**
+     * return false if the expression's syntax is invalid
+     * @param regularExpression the regular expression object to test
+     * @return  false if the expression's syntax is invalid
+     */
+    boolean isPatternValide( RegularExpression regularExpression )
+    {
+        return _bAvailable ? _service.isPatternValide( regularExpression ) : false;
+    }
+
+    /**
+    * return true if the value in parameter verify the pattern
+    * @param strValueToTest the value to test
+    * @param strPattern the regular expression Pattern
+    * @return true if the value in parameter verify the pattern
+    */
+    public boolean isMatches( String strValueToTest, String strPattern )
+    {
+        return _bAvailable ? _service.isMatches( strValueToTest, strPattern ) : false;
+    }
+
+    /**
+     * return true if the value in parameter verify the regular expression
+     * @param strValueToTest the value to test
+     * @param regularExpression the regular expression
+     * @return true if the value verify the regular expression
+     */
+    public boolean isMatches( String strValueToTest, RegularExpression regularExpression )
+    {
+        return _bAvailable ? _service.isMatches( strValueToTest, regularExpression ) : false;
+    }
+
+    /**
+     * return the regular expression object  whose identifier is specified in parameter
+     * @param nKey the regular expression key
+     * @return the regular expression object  whose identifier is specified in parameter
+     */
+    public RegularExpression getRegularExpressionByKey( int nKey )
+    {
+        return _bAvailable ? _service.getRegularExpressionByKey( nKey ) : null;
+    }
+
+    /**
+     * return a list of regular expression
+     * @return all regular expression
+     */
+    public List<RegularExpression> getAllRegularExpression(  )
+    {
+        return _bAvailable ? _service.getAllRegularExpression(  ) : null;
+    }
+}
