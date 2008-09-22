05de185511d8beb16b79910ac3d1b0101f5c2600
ELY <ELY@bab10101-e421-0410-a517-8ce0973de3ef>
2008-09-22 12:54:04 +0000

    LUTECE-870 Fixed : set default locale when user locale is english.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@9634 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/portal/service/i18n/I18nService.java
+++ src/java/fr/paris/lutece/portal/service/i18n/I18nService.java
@@ -61,6 +61,7 @@ public final class I18nService
     private static final String MARK_LOCALIZED_KEY_BEGIN = "#i18n{";
     private static final String MARK_LOCALIZED_KEY_END = "}";
     private static final String PROPERTY_AVAILABLES_LOCALES = "lutece.i18n.availableLocales";
+    private static final Locale LOCALE_DEFAULT = new Locale( "", "", "" );
     private static final String PROPERTY_DEFAULT_LOCALE = "lutece.i18n.defaultLocale";
     private static final String PROPERTY_FORMAT_DATE_SHORT_LIST = "lutece.format.date.short";
 
@@ -173,7 +174,7 @@ public final class I18nService
                 // bundle instead of the bundle of default locale.
                 if ( locale.getLanguage(  ).equals( Locale.ENGLISH.getLanguage(  ) ) )
                 {
-                    locale = getDefaultLocale(  );
+                    locale = LOCALE_DEFAULT;
                 }
 
                 ResourceBundle rbLabels = ResourceBundle.getBundle( strBundle, locale );
