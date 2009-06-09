7672259f3947f3dc833cf764204adbf5cf037911
tla-dev <tla-dev@users.noreply.github.com>
2009-06-09 09:54:09 +0000

    missing method correction
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@13193 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/portal/web/includes/ThemesInclude.java
+++ src/java/fr/paris/lutece/portal/web/includes/ThemesInclude.java
@@ -35,11 +35,17 @@ package fr.paris.lutece.portal.web.includes;
 
 import fr.paris.lutece.portal.service.content.PageData;
 import fr.paris.lutece.portal.service.includes.PageInclude;
-import fr.paris.lutece.portal.service.portal.ThemesService;
+import fr.paris.lutece.portal.service.util.AppPropertiesService;
+import fr.paris.lutece.util.ReferenceItem;
+import fr.paris.lutece.util.ReferenceList;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.StringTokenizer;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * This class provides  the list of the path associated by the topics of the page
@@ -50,6 +56,14 @@ public class ThemesInclude implements PageInclude
 
     private static final String MARK_THEME_CSS = "theme_css";
     private static final String MARK_THEME_IMAGES = "theme_images";
+    private static final String PROPERTY_PREFIX = "themes.";
+    private static final String PROPERTY_SUFFIX_NAME = ".name";
+    private static final String PROPERTY_SUFFIX_CSS = ".css";
+    private static final String PROPERTY_SUFFIX_IMAGES = ".images";
+    private static final String PROPERTY_THEMES_LIST = "themes.list";
+    private static final String DEFAULT_THEME = "default";
+    private static final String COOKIE_NAME = "theme";
+    private static String _strGlobalTheme = DEFAULT_THEME;
 
     /**
      * Substitue specific Freemarker markers in the page template.
@@ -64,23 +78,120 @@ public class ThemesInclude implements PageInclude
         String strTheme = data.getTheme();
 
         // The theme of the user
-        String strUserTheme = ThemesService.getUserTheme(request);
+        String strUserTheme = getUserTheme(request);
         if (strUserTheme != null)
         {
             strTheme = strUserTheme;
         }
 
         // If code_theme is null, used the default files ( css and images )
-        if ((strTheme == null) || (strTheme.equals("") || (strTheme.equals(ThemesService.DEFAULT_THEME))))
+        if ((strTheme == null) || (strTheme.equals("") || (strTheme.equals(DEFAULT_THEME))))
         {
-            strTheme = ThemesService.getGlobalTheme();
+            strTheme = _strGlobalTheme;
         }
 
-        String strCss = ThemesService.getThemeCSS(strTheme);
+        String strCss = AppPropertiesService.getProperty(PROPERTY_PREFIX + strTheme + PROPERTY_SUFFIX_CSS);
         rootModel.put(MARK_THEME_CSS, strCss);
 
-        String strImages = ThemesService.getThemeImages(strTheme);
+        String strImages = AppPropertiesService.getProperty(PROPERTY_PREFIX + strTheme + PROPERTY_SUFFIX_IMAGES);
         rootModel.put(MARK_THEME_IMAGES, strImages);
     }
 
+    /**
+     * Returns the list of the code_theme of the page
+     *
+     * @return the list of the page Code_theme in form of ReferenceList
+     */
+    public static ReferenceList getThemesList()
+    {
+        // recovers themes list from the includes.list entry in the properties download file
+        String strThemesList = AppPropertiesService.getProperty(PROPERTY_THEMES_LIST);
+
+        // extracts each item (separated by a comma) from the includes list
+        StringTokenizer strTokens = new StringTokenizer(strThemesList, ",");
+
+        ReferenceList listThemes = new ReferenceList();
+
+        while (strTokens.hasMoreTokens())
+        {
+            String strTheme = (String) strTokens.nextToken();
+            String strThemeName = AppPropertiesService.getProperty(PROPERTY_PREFIX + strTheme + PROPERTY_SUFFIX_NAME);
+            listThemes.addItem(strTheme, strThemeName);
+        }
+
+        return listThemes;
+    }
+
+    /**
+     * Gets the theme selected by the user
+     * @param request The HTTP request
+     * @return The theme if available otherwise null
+     */
+    private static String getUserTheme(HttpServletRequest request)
+    {
+        if (request != null)
+        {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null)
+            {
+                for (int i = 0; i < cookies.length; i++)
+                {
+                    Cookie cookie = cookies[i];
+                    if (cookie.getName().equalsIgnoreCase(COOKIE_NAME))
+                    {
+                        String strTheme = cookie.getValue();
+                        if (isValidTheme(strTheme))
+                        {
+                            return strTheme;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+
+    }
+
+    public static void setUserTheme(HttpServletRequest request, HttpServletResponse response, String strTheme)
+    {
+        Cookie cookie = new Cookie(COOKIE_NAME, strTheme);
+        response.addCookie(cookie);
+    }
+
+    /**
+     * Checks if the theme is among existing themes
+     * @param strTheme The theme to check
+     * @return True if the theme is valid
+     */
+    private static boolean isValidTheme(String strTheme)
+    {
+        Iterator i = getThemesList().iterator();
+        while (i.hasNext())
+        {
+            ReferenceItem item = (ReferenceItem) i.next();
+            if (item.getCode().equals(strTheme))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the global theme
+     * @return the global theme
+     */
+    public static String getGlobalTheme()
+    {
+        return _strGlobalTheme;
+    }
+
+    /**
+     * Sets the global theme
+     * @param strTheme The global theme
+     */
+    public static void setGlobalTheme(String strTheme)
+    {
+        _strGlobalTheme = strTheme;
+    }
 }
