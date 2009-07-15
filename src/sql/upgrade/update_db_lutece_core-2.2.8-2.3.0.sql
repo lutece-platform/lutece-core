3e17dc7846954a6f4b23135d9973ed2294ab39df
YCH <YCH@bab10101-e421-0410-a517-8ce0973de3ef>
2009-07-15 12:45:45 +0000

    LUTECE-1060 : Update SQL script for incremental indexation
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@13723 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/upgrade/update_db_lutece_core-2.2.8-2.3.0.sql
+++ src/sql/upgrade/update_db_lutece_core-2.2.8-2.3.0.sql
@@ -1,3 +1,13 @@
 ALTER TABLE core_style MODIFY COLUMN description_style VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
 
 INSERT INTO core_admin_right VALUES ('CORE_LINK_SERVICE_MANAGEMENT','portal.insert.adminFeature.linkService_management.name',2,NULL,'portal.insert.adminFeature.linkService_management.description',0,NULL,NULL,NULL,NULL,1);
+
+DROP TABLE IF EXISTS core_indexer_action;
+CREATE TABLE  core_indexer_action (
+  id_action int default 0 NOT NULL,
+  id_document varchar(255) NOT NULL,
+  id_task int default 0 NOT NULL,
+  indexer_name varchar(255) NOT NULL,
+  id_portlet int default 0 NOT NULL,
+  PRIMARY KEY (id_action)
+);
