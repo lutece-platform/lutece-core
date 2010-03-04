bea110d21f4f3e9c9b1fbc4903850b4b70aab684
V-V- <V-V-@users.noreply.github.com>
2010-03-04 14:22:54 +0000

    Unit Test : Corrected HtmlCleanerServiceTest removing unneeded characters to match expected result.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@19128 bab10101-e421-0410-a517-8ce0973de3ef

--- src/test/java/fr/paris/lutece/portal/service/html/HtmlCleanerServiceTest.java
+++ src/test/java/fr/paris/lutece/portal/service/html/HtmlCleanerServiceTest.java
@@ -53,7 +53,7 @@ public class HtmlCleanerServiceTest extends LuteceTestCase
 
         // Add double quote to attribute's values
         // Add ending tags
-        String expResult = "<p><font color=\"red\">Hello, World</font></p>\r\n";
+        String expResult = "<p><font color=\"red\">Hello, World</font></p>";
         String result = HtmlCleanerService.clean( strSource );
         assertNotNull( result );
         assertTrue( result.contains( expResult ) );
