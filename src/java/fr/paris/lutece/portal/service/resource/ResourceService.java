0e32b17f20e2b7ed9394dbe563bbbb313a16f092
pierrelevy <pierrelevy@users.noreply.github.com>
2011-03-17 00:07:30 +0000

    LUTECE-1303 : fix
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@27376 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/portal/service/resource/ResourceService.java
+++ src/java/fr/paris/lutece/portal/service/resource/ResourceService.java
@@ -69,7 +69,7 @@ public abstract class ResourceService extends AbstractCacheableService
         if ( ( strLoadersProperty != null ) && ( !strLoadersProperty.equals( "" ) ) )
         {
             initLoaders( strLoadersProperty );
-            initCache( getName() );
+//            initCache( getName() );
         }
         else
         {
