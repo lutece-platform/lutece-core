1367686ec74256b30a4f8077b99c02d726b00255
pierrelevy <pierrelevy@users.noreply.github.com>
2011-07-20 13:07:53 +0000

    LUTECE-1378 : New API to authenticate and validate request
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@32364 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/util/http/RequestAuthenticator.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2002-2011, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.util.http;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.httpclient.HttpMethodBase;
+
+/**
+ * Request Authenticator Interface
+ * @since v3.0
+ */
+public interface RequestAuthenticator
+{
+    /**
+     * Check the Authentication of a request
+     * @param request The HTTP request
+     * @return true if authenticated, otherwise false
+     */
+    boolean isRequestAuthenticated(HttpServletRequest request);
+    
+    /**
+     * Authenticate a request
+     * @param method The HTTP method to authenticate
+     * @param elements List of elements to include in the signature
+     */
+    void authenticateRequest( HttpMethodBase method , List<String> elements );
+    
+}
