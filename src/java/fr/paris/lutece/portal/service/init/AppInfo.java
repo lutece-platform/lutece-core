714a201240863143acdf435744f423b548338420
V-V- <V-V-@users.noreply.github.com>
2011-09-28 18:44:14 +0000

    Update core version
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@35203 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/portal/service/init/AppInfo.java
+++ src/java/fr/paris/lutece/portal/service/init/AppInfo.java
@@ -40,7 +40,7 @@ package fr.paris.lutece.portal.service.init;
 public final class AppInfo
 {
     /** Defines the current version of the application */
-    private static final String APP_VERSION = "3.0.3";
+    private static final String APP_VERSION = "3.0.4";
 
     /**
      * Creates a new AppInfo object.
