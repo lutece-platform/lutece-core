8419972b3e4c8d8c94c7489506ca599c037b1f9f
hohll <hohll@users.noreply.github.com>
2012-05-11 13:03:36 +0000

    Add jquery markitup editor (Html, BBCode, Wiki) to front office
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@40312 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ webapp/js/editors/markitup/sets/bbcode/set.js
@@ -0,0 +1,82 @@
+// ----------------------------------------------------------------------------
+// markItUp!
+// ----------------------------------------------------------------------------
+// Copyright (C) 2008 Jay Salvat
+// http://markitup.jaysalvat.com/
+// ----------------------------------------------------------------------------
+// BBCode tags example
+// http://en.wikipedia.org/wiki/Bbcode
+// ----------------------------------------------------------------------------
+// Feel free to add more tags
+// ----------------------------------------------------------------------------
+mySettings = {
+	previewParserPath:	'', // path to your BBCode parser
+	previewParser: function(content){
+	search = new Array(
+      /\[b\](.*?)\[\/b\]/,  
+	  /\[u\](.*?)\[\/u\]/,  
+      /\[i\](.*?)\[\/i\]/,
+      /\[img\](.*?)\[\/img\]/,
+      /\[url\="?(.*?)"?\](.*?)\[\/url\]/,
+      /\[quote](.*?)\[\/quote\]/,
+      /\[list\=(.*?)\](.*?)\[\/list\]/i,
+      /\[color\="?(.*?)"?\](.*?)\[\/color\]/,
+      /\[list\]([\s\S]*?)\[\/list\]/i,
+      /\[\*\]\s?(.*?)\n/);
+	replace = new Array(
+      "<strong>$1</strong>",
+      "<u>$1</u>",
+      "<em>$1</em>",
+      "<img src=\"$1\" alt=\"image\">",
+      "<a href=\"$1\">$2</a>",
+      "<blockquote>$1</blockquote>",
+      "<ol>$2</ol>",
+      "<span style=\"color:$1;\">$2</span>",
+      "<li>$1</li>");
+	for (i = 0; i < search.length; i++) {
+		content = content.replace(search[i], replace[i]);
+	}
+	return content;}, 
+	markupSet: [
+		{name:'Bold', key:'B', openWith:'[b]', closeWith:'[/b]'},
+		{name:'Italic', key:'I', openWith:'[i]', closeWith:'[/i]'},
+		{name:'Underline', key:'U', openWith:'[u]', closeWith:'[/u]'},
+		{separator:'---------------' },
+		{name:'Link', key:'L', openWith:'[url=[![Url]!]]', closeWith:'[/url]', placeHolder:'Your text to link here...'},
+		{separator:'---------------' },
+		{	name:'Colors', 
+			className:'colors', 
+			openWith:'[color=[![Color]!]]', 
+			closeWith:'[/color]', 
+				dropMenu: [
+					{name:'Yellow',	openWith:'[color=yellow]', 	closeWith:'[/color]', className:"col1-1" },
+					{name:'Orange',	openWith:'[color=orange]', 	closeWith:'[/color]', className:"col1-2" },
+					{name:'Red', 	openWith:'[color=red]', 	closeWith:'[/color]', className:"col1-3" },
+					
+					{name:'Blue', 	openWith:'[color=blue]', 	closeWith:'[/color]', className:"col2-1" },
+					{name:'Purple', openWith:'[color=purple]', 	closeWith:'[/color]', className:"col2-2" },
+					{name:'Green', 	openWith:'[color=green]', 	closeWith:'[/color]', className:"col2-3" },
+					
+					{name:'White', 	openWith:'[color=white]', 	closeWith:'[/color]', className:"col3-1" },
+					{name:'Gray', 	openWith:'[color=gray]', 	closeWith:'[/color]', className:"col3-2" },
+					{name:'Black',	openWith:'[color=black]', 	closeWith:'[/color]', className:"col3-3" }
+				]
+		},
+		{separator:'---------------' },
+		{name:'Size', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',
+		dropMenu :[
+			{name:'Big', openWith:'[size=200]', closeWith:'[/size]' },
+			{name:'Normal', openWith:'[size=100]', closeWith:'[/size]' },
+			{name:'Small', openWith:'[size=50]', closeWith:'[/size]' }
+		]},
+		{separator:'---------------' },
+		{name:'Bulleted list', openWith:'[list]\n', closeWith:'\n[/list]'},
+		{name:'Numeric list', openWith:'[list=[![Starting number]!]]\n', closeWith:'\n[/list]'}, 
+		{name:'List item', openWith:'[*] '},
+		{separator:'---------------' },
+		{name:'Code', openWith:'[code]', closeWith:'[/code]'}, 
+		{separator:'---------------' },
+		{name:'Clean', className:"clean", replaceWith:function(markitup) { return markitup.selection.replace(/\[(.*?)\]/g, "") } },
+		{name:'Preview', className:"preview", call:'preview' }
+	]
+}
