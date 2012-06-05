edb2714b241bdf4c1b8930ecb5095467e9fb2a3a
hohll <hohll@users.noreply.github.com>
2012-06-05 13:20:34 +0000

    Update markitup editor: - Add emoticons
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@41089 bab10101-e421-0410-a517-8ce0973de3ef

--- webapp/js/editors/markitup/sets/bbcode/set.js
+++ webapp/js/editors/markitup/sets/bbcode/set.js
@@ -9,49 +9,52 @@
 // ----------------------------------------------------------------------------
 // Feel free to add more tags
 // ----------------------------------------------------------------------------
+
+
 mySettings = {
 	previewParserPath:	'~/../../../parser/parserBbcode', // path to your BBCode parser
 	previewParser:false, 
 	markupSet: [
-		{name:'Bold', key:'B', openWith:'[b]', closeWith:'[/b]'},
-		{name:'Italic', key:'I', openWith:'[i]', closeWith:'[/i]'},
-		{name:'Underline', key:'U', openWith:'[u]', closeWith:'[/u]'},
+		{name:'Gras', key:'B', openWith:'[b]', closeWith:'[/b]'},
+		{name:'Italique', key:'I', openWith:'[i]', closeWith:'[/i]'},
+		{name:'Souligner', key:'U', openWith:'[u]', closeWith:'[/u]'},
 		{separator:'---------------' },
-		{name:'Link', key:'L', openWith:'[url=[![Url]!]]', closeWith:'[/url]', placeHolder:'Your text to link here...'},
+		{name:'Liens', key:'L', openWith:'[url=[![Url]!]]', closeWith:'[/url]', placeHolder:'Your text to link here...'},
 		{separator:'---------------' },
-		{	name:'Colors', 
+		{	name:'Couleurs', 
 			className:'colors', 
 			openWith:'[color=[![Color]!]]', 
 			closeWith:'[/color]', 
 				dropMenu: [
-					{name:'Yellow',	openWith:'[color=yellow]', 	closeWith:'[/color]', className:"col1-1" },
+					{name:'Jaune',	openWith:'[color=yellow]', 	closeWith:'[/color]', className:"col1-1" },
 					{name:'Orange',	openWith:'[color=orange]', 	closeWith:'[/color]', className:"col1-2" },
-					{name:'Red', 	openWith:'[color=red]', 	closeWith:'[/color]', className:"col1-3" },
+					{name:'Rouge', 	openWith:'[color=red]', 	closeWith:'[/color]', className:"col1-3" },
 					
-					{name:'Blue', 	openWith:'[color=blue]', 	closeWith:'[/color]', className:"col2-1" },
-					{name:'Purple', openWith:'[color=purple]', 	closeWith:'[/color]', className:"col2-2" },
-					{name:'Green', 	openWith:'[color=green]', 	closeWith:'[/color]', className:"col2-3" },
+					{name:'Bleu', 	openWith:'[color=blue]', 	closeWith:'[/color]', className:"col2-1" },
+					{name:'Violet', openWith:'[color=purple]', 	closeWith:'[/color]', className:"col2-2" },
+					{name:'Vert', 	openWith:'[color=green]', 	closeWith:'[/color]', className:"col2-3" },
 					
-					{name:'White', 	openWith:'[color=white]', 	closeWith:'[/color]', className:"col3-1" },
-					{name:'Gray', 	openWith:'[color=gray]', 	closeWith:'[/color]', className:"col3-2" },
-					{name:'Black',	openWith:'[color=black]', 	closeWith:'[/color]', className:"col3-3" }
+					{name:'Blanc', 	openWith:'[color=white]', 	closeWith:'[/color]', className:"col3-1" },
+					{name:'Gris', 	openWith:'[color=gray]', 	closeWith:'[/color]', className:"col3-2" },
+					{name:'Noir',	openWith:'[color=black]', 	closeWith:'[/color]', className:"col3-3" }
 				]
 		},
 		{separator:'---------------' },
-		{name:'Size', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',
+		{name:'Taille', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',
 		dropMenu :[
-			{name:'Big', openWith:'[size=200]', closeWith:'[/size]' },
-			{name:'Normal', openWith:'[size=100]', closeWith:'[/size]' },
-			{name:'Small', openWith:'[size=50]', closeWith:'[/size]' }
+			{name:'Grand', openWith:'[size=50]', closeWith:'[/size]' },
+			{name:'Normal', openWith:'[size=30]', closeWith:'[/size]' },
+			{name:'Petit', openWith:'[size=10]', closeWith:'[/size]' }
 		]},
 		{separator:'---------------' },
-		{name:'Bulleted list', openWith:'[list]\n', closeWith:'\n[/list]'},
-		{name:'Numeric list', openWith:'[list=[![Starting number]!]]\n', closeWith:'\n[/list]'}, 
-		{name:'List item', openWith:'[*] '},
+		{name:'Liste &agrave; puce', openWith:'[list]\n', closeWith:'\n[/list]'},
+		{name:'Liste ordonn&eacute;e', openWith:'[list=[![Starting number]!]]\n', closeWith:'\n[/list]'}, 
+		{name:'Element de liste', openWith:'[*] '},
 		{separator:'---------------' },
 		{name:'Code', openWith:'[code]', closeWith:'[/code]'}, 
 		{separator:'---------------' },
-		{name:'Clean', className:"clean", replaceWith:function(markitup) { return markitup.selection.replace(/\[(.*?)\]/g, "") } },
-		{name:'Preview', className:"preview", call:'preview' }
+		{name:'Nettoyer', className:"clean", replaceWith:function(markitup) { return markitup.selection.replace(/\[(.*?)\]/g, "") } },
+		{name:'Pr&eacute;visualiser - Alt + Click pour supprimer la pr&eacute;visualisation', className:"preview", call:'preview' },
+		{separator:'---------------' }
 	]
 }
