9679ab0bb5e72b1e206eee80e174c0445e2e8eda
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2012-10-30 14:18:09 +0000

    LUTECE-1518:Modify Html Cleaner Service for giving the possibility to choose between differents htmlCleaner + add an implementation of JSOUP
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@44933 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/portal/service/html/JSoupHtmlCleaner.java
+++ src/java/fr/paris/lutece/portal/service/html/JSoupHtmlCleaner.java
@@ -51,7 +51,7 @@ public class JSoupHtmlCleaner implements IHtmlCleaner
     @Override
     public String clean( String strSource ) throws HtmlCleanerException
     {
-        String strSafe = Jsoup.clean( strSource, Whitelist.basicWithImages(  ) );
+        String strSafe = Jsoup.clean( strSource, Whitelist.relaxed() );
 
         return strSafe;
     }
