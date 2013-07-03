6b3ff1466098cca898a4b61cfe283cb008c3ed80
pierrelevy <pierrelevy@users.noreply.github.com>
2013-07-03 20:42:22 +0000

    LUTECE-1612 : Popups Credits and LegalInfo improvements
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@49457 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/init_db_lutece_core.sql
+++ src/sql/init_db_lutece_core.sql
@@ -202,6 +202,10 @@ INSERT INTO core_datastore VALUES ('portal.site.site_property.email', '<webmaste
 INSERT INTO core_datastore VALUES ('portal.site.site_property.noreply_email', 'no-reply@mydomain.com');
 INSERT INTO core_datastore VALUES ('portal.site.site_property.home_url', 'jsp/site/Portal.jsp');
 INSERT INTO core_datastore VALUES ('portal.site.site_property.admin_home_url', 'jsp/admin/AdminMenu.jsp');
+INSERT INTO core_datastore VALUES ('portal.site.site_property.popup_credits.text', '&lt;credits text&gt;');
+INSERT INTO core_datastore VALUES ('portal.site.site_property.popup_legal_info.copyright.text', '&lt;copyright text&gt;');
+INSERT INTO core_datastore VALUES ('portal.site.site_property.popup_legal_info.privacy.text', '&lt;privacy text&gt;');
+
 
 INSERT INTO core_search_parameter (parameter_key, parameter_value) VALUES ('type_filter', 'none');
 INSERT INTO core_search_parameter (parameter_key, parameter_value) VALUES ('default_operator', 'OR');
