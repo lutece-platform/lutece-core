abd00abb939033f89cc5141396bf054dde082210
hohll <hohll@users.noreply.github.com>
2014-10-16 15:29:27 +0000

    LUTECE-1806 : Improve back office dashboard update admin.js
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@71721 bab10101-e421-0410-a517-8ce0973de3ef

--- webapp/js/admin.js
+++ webapp/js/admin.js
@@ -5,23 +5,6 @@
 $(function() {
     "use strict";
 
-    //Enable sidebar toggle
-    $("[data-toggle='offcanvas']").click(function(e) {
-        e.preventDefault();
-
-        //If window is small enough, enable sidebar push menu
-        if ($(window).width() <= 992) {
-            $('.row-offcanvas').toggleClass('active');
-            $('.left-side').removeClass("collapse-left");
-            $(".right-side").removeClass("strech");
-            $('.row-offcanvas').toggleClass("relative");
-        } else {
-            //Else, enable content streching
-            $('.left-side').toggleClass("collapse-left");
-            $(".right-side").toggleClass("strech");
-        }
-    });
-
     //Add hover support for touch devices
     $('.btn').bind('touchstart', function() {
         $(this).addClass('hover');
@@ -53,16 +36,7 @@ $(function() {
         }
     });
 
-    /*
-     * ADD SLIMSCROLL TO THE TOP NAV DROPDOWNS
-     * ---------------------------------------
-     */
-    $(".navbar .menu").slimscroll({
-        height: "200px",
-        alwaysVisible: false,
-        size: "3px"
-    }).css("width", "100%");
-
+   
     /*
      * INITIALIZE BUTTON TOGGLE
      * ------------------------
@@ -83,56 +57,34 @@ $(function() {
         box.slideUp();
     });
 
-    /* Sidebar tree view */
-    $(".sidebar .treeview").tree();
-
-    /* 
-     * Make sure that the sidebar is streched full height
-     * ---------------------------------------------
-     * We are gonna assign a min-height value every time the
-     * wrapper gets resized and upon page load. We will use
-     * Ben Alman's method for detecting the resize event.
-     * 
-     **/
-    function _fix() {
-        //Get window height and the wrapper height
-        var height = $(window).height() - $("body > .header").height() - ($("body > .footer").outerHeight() || 0);
-        $(".wrapper").css("min-height", height + "px");
-        var content = $(".wrapper").height();
-        //If the wrapper height is greater than the window
-        if (content > height)
-            //then set sidebar height to the wrapper
-            $(".left-side, html, body").css("min-height", content + "px");
-        else {
-            //Otherwise, set the sidebar to the height of the window
-            $(".left-side, html, body").css("min-height", height + "px");
-        }
-    }
-    //Fire upon load
-    _fix();
-    //Fire when wrapper is resized
-    $(".wrapper").resize(function() {
-        _fix();
-        fix_sidebar();
-    });
-
-    //Fix the fixed layout sidebar scroll bug
-    fix_sidebar();
-
-    /*
-     * We are gonna initialize all checkbox and radio inputs to 
-     * iCheck plugin in.
-     * You can find the documentation at http://fronteed.com/iCheck/
-     */
-    $("input[type='checkbox']:not(.simple), input[type='radio']:not(.simple)").iCheck({
-        checkboxClass: 'icheckbox_minimal',
-        radioClass: 'iradio_minimal'
-    });
-
+   
 });
 
 /* Specific script for back office */
 $( document ).ready(function( $ ) {
+
+	// Count effect 
+	$('.small-box .inner h3').each( function () {
+		var nCounter = $(this).text();
+		var sVal = "";
+		var thisTXT = $(this).text().split("/");
+		if ( thisTXT.length > 1 ){
+			nCounter = thisTXT[0];
+			sVal = " / " + thisTXT[1];
+		} 
+		if ( !isNaN( nCounter ) ) {
+			$(this).prop('Counter',0).animate({
+				Counter: nCounter
+			}, {
+				duration: 1000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text( Math.ceil(now) + sVal );
+				}
+			});
+		} 
+	});
+
 	if ( $("textarea#document_summary").length > 0 ){
 		// Limit to 250 the text in summary
 		var limit=250;
