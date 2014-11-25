abc749d1f5a2ac2f181dbf58648e20767939e316
hohll <hohll@users.noreply.github.com>
2014-11-25 18:05:40 +0000

    LUTECE-1806 : Set js to handle menu choose
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/core@72363 bab10101-e421-0410-a517-8ce0973de3ef

--- webapp/js/admin.js
+++ webapp/js/admin.js
@@ -88,49 +88,110 @@ $( document ).ready(function( $ ) {
 	// File Input Style 
 	$(":file").addClass("filestyle");
 	$(":file").filestyle({buttonText: "&nbsp;Parcourir"});
-	// Off Canvas Menu Begin
-	// Calling the function
-	$(function() {
-		$('.toggle-nav').click(function( e ) {
-			toggleNavigation();
-			e.preventDefault();
-		});
-		$('#nav>h2#title').click(function( e ) {
-			toggleNavigation();
-		});
-	});
-
-	// The toggleNav function itself
-	function toggleNavigation() {
-		if ($('#admin-wrapper').hasClass('display-nav')) {
-			// Close Nav
-			$('#admin-wrapper').removeClass('display-nav');
-		} else {
-			// Open Nav
-			$('#admin-wrapper').addClass('display-nav');
-		}
+	
+	/** MENU TO ACTIVATE **/
+	if( $("header").attr("data-menu")=="top" ){
+		$("#menu-top").attr("id","menu-right");
+	} else if( $("header").attr("data-menu")=="right") {
+		$("#top").toggle();
+		$("#menu-right").toggleClass("visible-xs");
+	} else {
+		$("#menu-left").toggleClass("visible-xs");
+		$("#top").toggle();
 	}
+		
+	/** MENU RIGHT **/
+			var $lateral_menu_trigger = $('#menu-right'),$content_wrapper = $('#admin-wrapper'),$navigation = $('header');
 
-	// Sliding codes
-	$("#toggle > li > div").click(function (e) {
-		e.preventDefault();
-		if (false == $(this).next().is(':visible')) {
-			$('#toggle ul').slideUp();
+			//open-close lateral menu clicking on the menu icon
+			$lateral_menu_trigger.on('click', function(event){
+				event.preventDefault();
+				
+				$lateral_menu_trigger.toggleClass('is-clicked');
+				$navigation.toggleClass('lateral-menu-is-open');
+				$content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					// firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+					$('body').toggleClass('overflow-hidden');
+				});
+				$('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+				
+				//check if transitions are not supported - i.e. in IE9
+				if($('html').hasClass('no-csstransitions')) {
+					$('body').toggleClass('overflow-hidden');
+				}
+			});
+
+			//close lateral menu clicking outside the menu itself
+			$content_wrapper.on('click', function(event){
+				if( !$(event.target).is('#menu-right, #menu-right span') ) {
+					$lateral_menu_trigger.removeClass('is-clicked');
+					$navigation.removeClass('lateral-menu-is-open');
+					$content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+						$('body').removeClass('overflow-hidden');
+					});
+					$('#cd-lateral-nav').removeClass('lateral-menu-is-open');
+					//check if transitions are not supported
+					if($('html').hasClass('no-csstransitions')) {
+						$('body').removeClass('overflow-hidden');
+					}
+
+				}
+			});
+
+			//open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+			$('.item-has-children').children('a').on('click', function(event){
+				event.preventDefault();
+				$(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+			});
+		
+		/**  **/		
+	
+		/** MENU LEFT **/	
+		// Off Canvas Menu Begin
+		// Calling the function
+		$(function() {
+			$('#menu-left').click(function( e ) {
+				toggleNavigation();
+				e.preventDefault();
+			});
+			
+			$('#nav>h2#title').click(function( e ) {
+				toggleNavigation();
+			});
+		});
+
+		// The toggleNav function itself
+		function toggleNavigation() {
+			if ($('#admin-wrapper').hasClass('display-nav')) {
+				// Close Nav
+				$('#admin-wrapper').removeClass('display-nav');
+			} else {
+				// Open Nav
+				$('#admin-wrapper').addClass('display-nav');
+			}
 		}
 
-		var $currIcon=$(this).find("span.the-btn");
+		// Sliding codes
+		$("#toggle > li > div").click(function (e) {
+			e.preventDefault();
+			if (false == $(this).next().is(':visible')) {
+				$('#toggle ul').slideUp();
+			}
 
-		$("span.the-btn").not($currIcon).addClass('fa-plus').removeClass('fa-minus');
+			var $currIcon=$(this).find("span.the-btn");
 
-		$currIcon.toggleClass('fa-minus fa-plus');
+			$("span.the-btn").not($currIcon).addClass('fa-plus').removeClass('fa-minus');
 
-		$(this).next().slideToggle();
+			$currIcon.toggleClass('fa-minus fa-plus');
 
-		$("#toggle > li > div").removeClass("active");
-		$(this).addClass('active');
+			$(this).next().slideToggle();
 
-	});
-	// Off Canvas Menu End
+			$("#toggle > li > div").removeClass("active");
+			$(this).addClass('active');
+
+		});
+		// Off Canvas Menu End
+		/**  **/	
 	
 	$(".portlet-type").on('click', function(e) {
 		// Stop the link default behaviour.
