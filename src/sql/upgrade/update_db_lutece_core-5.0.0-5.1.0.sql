4befd75a34e4d46237173fe89be49735b6e80562
Laurent HOHL <laurent.hohl@paris.fr>
2015-10-01 15:12:43 +0200

    LUTECE-1930 - TinyMCE 4.2.5 + sql update

---
+++ src/sql/upgrade/update_db_lutece_core-5.0.0-5.1.0.sql
@@ -0,0 +1,3 @@
+-- LUTECE 
+REPLACE INTO core_datastore VALUES ('portal.site.site_property.avatar_default', 'images/admin/skin/unknown.png');
+REPLACE INTO core_datastore VALUES ('portal.site.site_property.back_images', '\'images/admin/skin/bg_login1.jpg\' , \'images/admin/skin/bg_login.jpg\' , \'images/admin/skin/bg_login.jpg\' , \'images/admin/skin/bg_login.jpg\'');
