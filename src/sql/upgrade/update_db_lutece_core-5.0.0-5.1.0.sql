5c2ab7356b22ccd4482b5dbd800a6acab7672341
Jon Harper <jon.harper87@gmail.com>
2015-10-02 16:35:05 +0200

    LUTECE-1930 - fix default list of admin login background images in db

--- src/sql/upgrade/update_db_lutece_core-5.0.0-5.1.0.sql
+++ src/sql/upgrade/update_db_lutece_core-5.0.0-5.1.0.sql
@@ -1,3 +1,3 @@
 -- LUTECE 
 REPLACE INTO core_datastore VALUES ('portal.site.site_property.avatar_default', 'images/admin/skin/unknown.png');
-REPLACE INTO core_datastore VALUES ('portal.site.site_property.back_images', '\'images/admin/skin/bg_login1.jpg\' , \'images/admin/skin/bg_login.jpg\' , \'images/admin/skin/bg_login.jpg\' , \'images/admin/skin/bg_login.jpg\'');
+REPLACE INTO core_datastore VALUES ('portal.site.site_property.back_images', '\'images/admin/skin/bg_login1.jpg\' , \'images/admin/skin/bg_login2.jpg\' , \'images/admin/skin/bg_login3.jpg\' , \'images/admin/skin/bg_login4.jpg\'');
