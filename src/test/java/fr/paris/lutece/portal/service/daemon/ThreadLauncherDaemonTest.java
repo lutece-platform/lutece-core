023ef7a5585877dd3ff271f72140e915288cd2b1
Rémi Zara <remi.zara@paris.fr>
2017-03-18 19:00:06 +0100

    LUTECE-2085 : Make the threadLauncherDaemon reactive
    Have the daemon signaled when the runnable ends.
    
    This allows dead threads reclaiming, and execution of delayed runnable
    which where blocked from execution because no slot was available for their
    key.

---
+++ src/test/java/fr/paris/lutece/portal/service/daemon/ThreadLauncherDaemonTest.java
@@ -0,0 +1,32 @@
+package fr.paris.lutece.portal.service.daemon;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import fr.paris.lutece.portal.service.plugin.PluginService;
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class ThreadLauncherDaemonTest extends LuteceTestCase
+{
+    private boolean _runnableTimedOut;
+
+    public void testAddItemToQueue( ) throws InterruptedException, BrokenBarrierException, TimeoutException
+    {
+        CyclicBarrier barrier = new CyclicBarrier( 2 );
+        _runnableTimedOut = false;
+        ThreadLauncherDaemon.addItemToQueue( ( ) -> {
+            try
+            {
+                barrier.await( 10L, TimeUnit.SECONDS );
+            }
+            catch ( InterruptedException | BrokenBarrierException | TimeoutException e )
+            {
+                _runnableTimedOut = true;
+            }
+        }, "key", PluginService.getCore( ) );
+        barrier.await( 250L, TimeUnit.MILLISECONDS );
+        assertFalse( _runnableTimedOut );
+    }
+}
