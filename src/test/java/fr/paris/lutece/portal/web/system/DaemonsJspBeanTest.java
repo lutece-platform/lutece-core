dcd753253cdffad43b55b497f7e3fb9c854db444
Jon Harper <jon.harper87@gmail.com>
2017-06-02 15:22:08 +0200

    LUTECE-2085 : DaemonsJspBeanTest, add a test of the schedule interval value. Add logs to better diagnose failures

--- src/test/java/fr/paris/lutece/portal/web/system/DaemonsJspBeanTest.java
+++ src/test/java/fr/paris/lutece/portal/web/system/DaemonsJspBeanTest.java
@@ -50,6 +50,7 @@ import fr.paris.lutece.portal.service.daemon.DaemonEntry;
 import fr.paris.lutece.portal.service.daemon.TestDaemon;
 import fr.paris.lutece.portal.service.datastore.DatastoreService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
+import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.test.LuteceTestCase;
 
 public class DaemonsJspBeanTest extends LuteceTestCase
@@ -128,17 +129,35 @@ public class DaemonsJspBeanTest extends LuteceTestCase
 
     public void testDoDaemonActionStart( ) throws InterruptedException, BrokenBarrierException, TimeoutException
     {
+        long lReadyTime;
+        long lScheduledTime;
         assertFalse( _entry.isRunning( ) );
         MockHttpServletRequest request = new MockHttpServletRequest( );
         request.setParameter( "action", "START" );
         request.setParameter( "daemon", JUNIT_DAEMON );
+        lReadyTime = System.nanoTime( );
         bean.doDaemonAction( request ); // Daemon should run periodically with interval of 1s
+
         assertTrue( _entry.isRunning( ) );
         TestDaemon daemon = (TestDaemon) AppDaemonService.getDaemon( JUNIT_DAEMON );
         daemon.go( );
+        lScheduledTime = System.nanoTime( );
+        AppLogService.info( "Daemon scheduled after " + ( ( lScheduledTime - lReadyTime ) / 1000000 ) + " ms" );
+
         daemon.waitForCompletion( ); // Complete first run without a timeout
+
+        lReadyTime = System.nanoTime( );
         daemon.go( );
+        lScheduledTime = System.nanoTime( );
+        long lRescheduleDurationMilliseconds = ( ( lScheduledTime - lReadyTime ) / 1000000 );
+        long lMarginMilliseconds = 250;
+        AppLogService.info( "Daemon scheduled after " + lRescheduleDurationMilliseconds + " ms" );
+
         daemon.waitForCompletion( ); // Complete second run without a timeout. More runs would follow if we continued
+
+        assertTrue(
+                "Daemon should be re-scheduled approximately 1 second after the end of the previous run, but got " + lRescheduleDurationMilliseconds + "ms",
+                1000 - lMarginMilliseconds <= lRescheduleDurationMilliseconds && lRescheduleDurationMilliseconds <= 1000 + lMarginMilliseconds );
     }
 
     public void testDoDaemonActionStop( ) throws InterruptedException, BrokenBarrierException, TimeoutException
@@ -168,17 +187,30 @@ public class DaemonsJspBeanTest extends LuteceTestCase
 
     public void testDoDaemonActionRun( ) throws InterruptedException, BrokenBarrierException, TimeoutException
     {
+        long lReadyTime;
+        long lScheduledTime;
         assertFalse( _entry.isRunning( ) );
         _entry.setInterval( 1000 );
+
+        lReadyTime = System.nanoTime( );
         AppDaemonService.startDaemon( JUNIT_DAEMON ); // Daemon should run periodically with interval of 1000s
         TestDaemon daemon = (TestDaemon) AppDaemonService.getDaemon( JUNIT_DAEMON );
         daemon.go( );
+        lScheduledTime = System.nanoTime( );
+        AppLogService.info( "Daemon scheduled after " + ( ( lScheduledTime - lReadyTime ) / 1000000 ) + " ms" );
+
         daemon.waitForCompletion( );
+
         MockHttpServletRequest request = new MockHttpServletRequest( );
         request.setParameter( "action", "RUN" ); // Manually do 1 run of the daemon now
         request.setParameter( "daemon", JUNIT_DAEMON );
+        lReadyTime = System.nanoTime( );
         bean.doDaemonAction( request );
+
         daemon.go( ); // It should run in less than 1000 seconds !
+        lScheduledTime = System.nanoTime( );
+        AppLogService.info( "Daemon scheduled after " + ( ( lScheduledTime - lReadyTime ) / 1000000 ) + " ms" );
+
         daemon.waitForCompletion( );
     }
 
