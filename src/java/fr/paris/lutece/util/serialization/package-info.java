1fe3ff13e0ce62e4c3eded241001241bc2533c7e
Jon Harper <jon.harper87@gmail.com>
2017-08-04 19:44:30 +0200

    LUTECE-2140 : Add shaded commons io ValidatingObjectInputStream and needed classes for lutece 5

---
+++ src/java/fr/paris/lutece/util/serialization/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Shaded package from apache commons-io 2.5 org.apache.commons.io.serialization..
+ * To be used when using older versions of commons-io.
+ * see http://commons.apache.org/proper/commons-io/
+ */
+package fr.paris.lutece.util.serialization;
