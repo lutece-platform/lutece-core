d55aae260ae93537154b61e772d5bdce9137c43c
Rémi Zara <remi.zara@paris.fr>
2017-10-15 11:10:16 +0200

    LUTECE-2164 : Add a CSRF token to indexation actions
    Add a way to unregister an indexer, to allow adding and removing
    a test indexer.

--- src/test/java/fr/paris/lutece/portal/web/search/SearchIndexationJspBeanTest.java
+++ src/test/java/fr/paris/lutece/portal/web/search/SearchIndexationJspBeanTest.java
@@ -33,10 +33,20 @@
  */
 package fr.paris.lutece.portal.web.search;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.lucene.document.Document;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import fr.paris.lutece.portal.business.user.AdminUser;
 import fr.paris.lutece.portal.service.admin.AccessDeniedException;
+import fr.paris.lutece.portal.service.daemon.AppDaemonService;
+import fr.paris.lutece.portal.service.daemon.DaemonEntry;
+import fr.paris.lutece.portal.service.message.SiteMessageException;
+import fr.paris.lutece.portal.service.search.IndexationService;
+import fr.paris.lutece.portal.service.search.SearchIndexer;
+import fr.paris.lutece.portal.service.security.SecurityTokenService;
 import fr.paris.lutece.test.LuteceTestCase;
 import fr.paris.lutece.test.Utils;
 
@@ -46,33 +56,162 @@ import fr.paris.lutece.test.Utils;
  */
 public class SearchIndexationJspBeanTest extends LuteceTestCase
 {
+    private TestSearchIndexer testIndexer;
+    private Boolean bIndexDaemonInitialState;
+
+    private static final class TestSearchIndexer implements SearchIndexer
+    {
+        private boolean _bIndexDocumentsCalled;
+
+        @Override
+        public boolean isEnable( )
+        {
+            return true;
+        }
+
+        @Override
+        public void indexDocuments( ) throws IOException, InterruptedException, SiteMessageException
+        {
+            _bIndexDocumentsCalled = true;
+        }
+
+        @Override
+        public String getVersion( )
+        {
+            return "1.0.0";
+        }
+
+        @Override
+        public String getSpecificSearchAppUrl( )
+        {
+            return null;
+        }
+
+        @Override
+        public String getName( )
+        {
+            return this.getClass( ).getCanonicalName( );
+        }
+
+        @Override
+        public List<String> getListType( )
+        {
+            return null;
+        }
+
+        @Override
+        public List<Document> getDocuments( String strIdDocument )
+                throws IOException, InterruptedException, SiteMessageException
+        {
+            return null;
+        }
+
+        @Override
+        public String getDescription( )
+        {
+            return "junit test indexer";
+        }
+    }
+
+    @Override
+    protected void setUp( ) throws Exception
+    {
+        super.setUp( );
+        // we stop the indexer daemon so that it does not interfere with the
+        // tests
+        for ( DaemonEntry daemonEntry : AppDaemonService.getDaemonEntries( ) )
+        {
+            if ( daemonEntry.getId( ).equals( "indexer" ) )
+            {
+                bIndexDaemonInitialState = daemonEntry.isRunning( );
+                break;
+            }
+        }
+        assertNotNull( "Did not find indexer daemon", bIndexDaemonInitialState );
+        AppDaemonService.stopDaemon( "indexer" );
+        testIndexer = new TestSearchIndexer( );
+        IndexationService.registerIndexer( testIndexer );
+
+    }
+
+    @Override
+    protected void tearDown( ) throws Exception
+    {
+        IndexationService.unregisterIndexer( testIndexer );
+        if ( bIndexDaemonInitialState.booleanValue( ) )
+        {
+            AppDaemonService.startDaemon( "indexer" );
+        }
+        super.tearDown( );
+    }
+
     /**
-     * Test of getIndexingProperties method, of class fr.paris.lutece.portal.web.search.SearchIndexationJspBean.
+     * Test of getIndexingProperties method, of class
+     * fr.paris.lutece.portal.web.search.SearchIndexationJspBean.
      */
     public void testGetIndexingProperties( ) throws AccessDeniedException
     {
-        System.out.println( "getIndexingProperties" );
-
         MockHttpServletRequest request = new MockHttpServletRequest( );
         Utils.registerAdminUserWithRigth( request, new AdminUser( ), SearchIndexationJspBean.RIGHT_INDEXER );
 
         SearchIndexationJspBean instance = new SearchIndexationJspBean( );
         instance.init( request, SearchIndexationJspBean.RIGHT_INDEXER );
-        instance.getIndexingProperties( request );
+        assertNotNull( instance.getIndexingProperties( request ) );
     }
 
     /**
-     * Test of doIndexing method, of class fr.paris.lutece.portal.web.search.SearchIndexationJspBean.
+     * Test of doIndexing method, of class
+     * fr.paris.lutece.portal.web.search.SearchIndexationJspBean.
      */
     public void testDoIndexing( ) throws AccessDeniedException
     {
-        System.out.println( "doIndexing" );
-
         MockHttpServletRequest request = new MockHttpServletRequest( );
         SearchIndexationJspBean instance = new SearchIndexationJspBean( );
+        request.addParameter( SecurityTokenService.PARAMETER_TOKEN,
+                SecurityTokenService.getInstance( ).getToken( request, "admin/search/manage_search_indexation.html" ) );
 
         Utils.registerAdminUserWithRigth( request, new AdminUser( ), SearchIndexationJspBean.RIGHT_INDEXER );
         instance.init( request, SearchIndexationJspBean.RIGHT_INDEXER );
         instance.doIndexing( request );
+        assertTrue( testIndexer._bIndexDocumentsCalled );
+    }
+
+    public void testDoIndexingInvalidToken( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        SearchIndexationJspBean instance = new SearchIndexationJspBean( );
+        request.addParameter( SecurityTokenService.PARAMETER_TOKEN,
+                SecurityTokenService.getInstance( ).getToken( request, "admin/search/manage_search_indexation.html" )
+                        + "b" );
+
+        Utils.registerAdminUserWithRigth( request, new AdminUser( ), SearchIndexationJspBean.RIGHT_INDEXER );
+        instance.init( request, SearchIndexationJspBean.RIGHT_INDEXER );
+        try
+        {
+            instance.doIndexing( request );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            assertFalse( testIndexer._bIndexDocumentsCalled );
+        }
+    }
+
+    public void testDoIndexingNoToken( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        SearchIndexationJspBean instance = new SearchIndexationJspBean( );
+
+        Utils.registerAdminUserWithRigth( request, new AdminUser( ), SearchIndexationJspBean.RIGHT_INDEXER );
+        instance.init( request, SearchIndexationJspBean.RIGHT_INDEXER );
+        try
+        {
+            instance.doIndexing( request );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            assertFalse( testIndexer._bIndexDocumentsCalled );
+        }
     }
 }
