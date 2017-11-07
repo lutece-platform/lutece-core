2e2dc21d966a9aefc5d1d71c93f928a8ed7b6167
Rémi Zara <remi.zara@paris.fr>
2017-11-07 20:24:57 +0100

    LUTECE-2170 : Add a CSRF token to admin dashboard move

--- src/java/fr/paris/lutece/portal/web/dashboard/AdminDashboardJspBean.java
+++ src/java/fr/paris/lutece/portal/web/dashboard/AdminDashboardJspBean.java
@@ -36,11 +36,13 @@ package fr.paris.lutece.portal.web.dashboard;
 import fr.paris.lutece.portal.business.dashboard.AdminDashboardFactory;
 import fr.paris.lutece.portal.business.dashboard.AdminDashboardHome;
 import fr.paris.lutece.portal.business.user.AdminUser;
+import fr.paris.lutece.portal.service.admin.AccessDeniedException;
 import fr.paris.lutece.portal.service.admin.AdminUserService;
 import fr.paris.lutece.portal.service.dashboard.admin.AdminDashboardService;
 import fr.paris.lutece.portal.service.dashboard.admin.IAdminDashboardComponent;
 import fr.paris.lutece.portal.service.message.AdminMessage;
 import fr.paris.lutece.portal.service.message.AdminMessageService;
+import fr.paris.lutece.portal.service.security.SecurityTokenService;
 import fr.paris.lutece.portal.service.template.AppTemplateService;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.web.admin.AdminFeaturesPageJspBean;
@@ -141,6 +143,7 @@ public class AdminDashboardJspBean extends AdminFeaturesPageJspBean
         model.put( MARK_MAP_AVAILABLE_ORDERS, getMapAvailableOrders( ) );
         model.put( MARK_LIST_AVAILABLE_COLUMNS, getListAvailableColumns( ) );
         model.put( MARK_MAP_COLUMN_ORDER_STATUS, _service.getOrderedColumnsStatus( ) );
+        model.put( SecurityTokenService.MARK_TOKEN, SecurityTokenService.getInstance( ).getToken( request, TEMPLATE_MANAGE_DASHBOARDS ) );
 
         HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_MANAGE_DASHBOARDS, user.getLocale( ), model );
 
@@ -187,8 +190,10 @@ public class AdminDashboardJspBean extends AdminFeaturesPageJspBean
      * @param request
      *            the request
      * @return url
+     * @throws AccessDeniedException
+     *             if the security token is invalid
      */
-    public String doMoveAdminDashboard( HttpServletRequest request )
+    public String doMoveAdminDashboard( HttpServletRequest request ) throws AccessDeniedException
     {
         String strDashboardName = request.getParameter( PARAMETER_DASHBOARD_NAME );
 
@@ -223,6 +228,10 @@ public class AdminDashboardJspBean extends AdminFeaturesPageJspBean
         {
             nOldOrder = dashboard.getOrder( );
             nOldColumn = dashboard.getZone( );
+        }
+        if ( !SecurityTokenService.getInstance( ).validate( request, TEMPLATE_MANAGE_DASHBOARDS ) )
+        {
+            throw new AccessDeniedException( "Invalid security token" );
         }
 
         // set order and column
--- src/test/java/fr/paris/lutece/portal/web/dashboard/AdminDashboardJspBeanTest.java
+++ src/test/java/fr/paris/lutece/portal/web/dashboard/AdminDashboardJspBeanTest.java
@@ -33,13 +33,21 @@
  */
 package fr.paris.lutece.portal.web.dashboard;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import fr.paris.lutece.portal.business.dashboard.AdminDashboardFactory;
+import fr.paris.lutece.portal.business.dashboard.AdminDashboardHome;
 import fr.paris.lutece.portal.business.user.AdminUser;
 import fr.paris.lutece.portal.business.user.AdminUserHome;
 import fr.paris.lutece.portal.service.admin.AccessDeniedException;
 import fr.paris.lutece.portal.service.admin.AdminAuthenticationService;
-import fr.paris.lutece.portal.service.security.UserNotSignedException;
+import fr.paris.lutece.portal.service.admin.PasswordResetException;
+import fr.paris.lutece.portal.service.dashboard.admin.IAdminDashboardComponent;
+import fr.paris.lutece.portal.service.security.SecurityTokenService;
 import fr.paris.lutece.test.LuteceTestCase;
 import fr.paris.lutece.test.Utils;
 
@@ -50,22 +58,142 @@ import fr.paris.lutece.test.Utils;
 public class AdminDashboardJspBeanTest extends LuteceTestCase
 {
 
+    private AdminDashboardJspBean instance;
+    private IAdminDashboardComponent _dashboard;
+
+    @Override
+    protected void setUp( ) throws Exception
+    {
+        super.setUp( );
+        instance = new AdminDashboardJspBean( );
+        _dashboard = new TestAdminDashboardCompoent( );
+        _dashboard.setName( getRandomName( ) );
+        AdminDashboardFactory.registerDashboardComponent( _dashboard );
+        AdminDashboardHome.create( _dashboard );
+    }
+
+    @Override
+    protected void tearDown( ) throws Exception
+    {
+        AdminDashboardHome.remove( _dashboard.getName( ) );
+        // TODO : dashboard should be unregistered
+        super.tearDown( );
+    }
+
+    private String getRandomName( )
+    {
+        Random rand = new SecureRandom( );
+        BigInteger bigInt = new BigInteger( 128, rand );
+        return "junit" + bigInt.toString( 36 );
+    }
+
     /**
-     * Test of getAdminDashboards method, of class fr.paris.lutece.portal.web.dashboard.AdminDashboardJspBean.
-     * @throws Exception e
+     * Test of getAdminDashboards method, of class
+     * fr.paris.lutece.portal.web.dashboard.AdminDashboardJspBean.
+     * 
+     * @throws Exception
+     *             e
      */
     public void testGetAdminDashboards( ) throws Exception
     {
-        System.out.println( "getAdminDashboards" );
-
         MockHttpServletRequest request = new MockHttpServletRequest( );
 
         AdminUser user = AdminUserHome.findUserByLogin( "admin" );
-        AdminAuthenticationService.getInstance( ).registerUser( request, user  );
+        AdminAuthenticationService.getInstance( ).registerUser( request, user );
 
-        AdminDashboardJspBean instance = new AdminDashboardJspBean( );
+        instance = new AdminDashboardJspBean( );
 
-        //should not throw on freemarker templating
-        instance.getAdminDashboards( request );
+        // should not throw on freemarker templating
+        String adminDashboards = instance.getAdminDashboards( request );
+        assertNotNull( adminDashboards );
+        assertFalse( "".equals( adminDashboards ) );
+    }
+
+    public void testGetManageDashboards( ) throws PasswordResetException, AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        Utils.registerAdminUserWithRigth( request, new AdminUser( ),
+                AdminDashboardJspBean.RIGHT_MANAGE_ADMINDASHBOARD );
+
+        instance.init( request, AdminDashboardJspBean.RIGHT_MANAGE_ADMINDASHBOARD );
+
+        assertNotNull( instance.getManageDashboards( request ) );
+    }
+
+    public void testDoMoveAdminDashboard( ) throws AccessDeniedException
+    {
+        IAdminDashboardComponent stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+        assertNotNull( stored );
+        assertEquals( 0, stored.getOrder( ) );
+        assertEquals( 0, stored.getZone( ) );
+
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        request.setParameter( "dashboard_name", _dashboard.getName( ) );
+        request.setParameter( "dashboard_order", "-1" );
+        request.setParameter( "dashboard_column", "-1" );
+        request.setParameter( SecurityTokenService.PARAMETER_TOKEN, SecurityTokenService.getInstance( )
+                .getToken( request, "/admin/dashboard/admin/manage_dashboards.html" ) );
+
+        instance.doMoveAdminDashboard( request );
+
+        stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+        assertNotNull( stored );
+        assertEquals( 1, stored.getOrder( ) );
+        assertEquals( -1, stored.getZone( ) );
+    }
+
+    public void testDoMoveAdminDashboardInvalidToken( ) throws AccessDeniedException
+    {
+        IAdminDashboardComponent stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+        assertNotNull( stored );
+        assertEquals( 0, stored.getOrder( ) );
+        assertEquals( 0, stored.getZone( ) );
+
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        request.setParameter( "dashboard_name", _dashboard.getName( ) );
+        request.setParameter( "dashboard_order", "-1" );
+        request.setParameter( "dashboard_column", "-1" );
+        request.setParameter( SecurityTokenService.PARAMETER_TOKEN,
+                SecurityTokenService.getInstance( ).getToken( request, "/admin/dashboard/admin/manage_dashboards.html" )
+                        + "b" );
+
+        try
+        {
+            instance.doMoveAdminDashboard( request );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+            assertNotNull( stored );
+            assertEquals( 0, stored.getOrder( ) );
+            assertEquals( 0, stored.getZone( ) );
+        }
+    }
+
+    public void testDoMoveAdminDashboardNoToken( ) throws AccessDeniedException
+    {
+        IAdminDashboardComponent stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+        assertNotNull( stored );
+        assertEquals( 0, stored.getOrder( ) );
+        assertEquals( 0, stored.getZone( ) );
+
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        request.setParameter( "dashboard_name", _dashboard.getName( ) );
+        request.setParameter( "dashboard_order", "-1" );
+        request.setParameter( "dashboard_column", "-1" );
+
+        try
+        {
+            instance.doMoveAdminDashboard( request );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            stored = AdminDashboardHome.findByPrimaryKey( _dashboard.getName( ) );
+            assertNotNull( stored );
+            assertEquals( 0, stored.getOrder( ) );
+            assertEquals( 0, stored.getZone( ) );
+        }
     }
 }
