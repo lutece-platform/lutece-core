7c5a9244cd6aaeaa9bd1e2f079aef9278c193667
Rémi Zara <remi.zara@paris.fr>
2017-11-12 20:51:26 +0100

    LUTECE-2176 : Add a CSRF token to XslExport modification

--- src/test/java/fr/paris/lutece/portal/web/xsl/XslExportJspBeanTest.java
+++ src/test/java/fr/paris/lutece/portal/web/xsl/XslExportJspBeanTest.java
@@ -48,9 +48,13 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import fr.paris.lutece.portal.business.file.File;
+import fr.paris.lutece.portal.business.file.FileHome;
+import fr.paris.lutece.portal.business.physicalfile.PhysicalFile;
 import fr.paris.lutece.portal.business.rbac.AdminRole;
 import fr.paris.lutece.portal.business.rbac.AdminRoleHome;
 import fr.paris.lutece.portal.business.user.AdminUser;
+import fr.paris.lutece.portal.business.xsl.XslExport;
 import fr.paris.lutece.portal.business.xsl.XslExportHome;
 import fr.paris.lutece.portal.service.admin.AccessDeniedException;
 import fr.paris.lutece.portal.service.security.SecurityTokenService;
@@ -61,12 +65,37 @@ import fr.paris.lutece.test.Utils;
 public class XslExportJspBeanTest extends LuteceTestCase
 {
     private XslExportJspBean _instance;
+    private XslExport _xslExport;
 
     @Override
     protected void setUp( ) throws Exception
     {
         super.setUp( );
         _instance = new XslExportJspBean( );
+        String strName = getRandomName( );
+        _xslExport = new XslExport( );
+        _xslExport.setTitle( strName );
+        _xslExport.setDescription( strName );
+        _xslExport.setExtension( strName );
+        _xslExport.setPlugin( "" );
+        File file = new File( );
+        PhysicalFile physicalFile = new PhysicalFile( );
+        physicalFile.setValue( new byte[ 1 ] );
+        file.setTitle( strName );
+        file.setSize( 1 );
+        file.setPhysicalFile( physicalFile );
+        file.setMimeType( "application/xml" );
+        _xslExport.setFile( file );
+        _xslExport.getFile( ).setIdFile( FileHome.create( _xslExport.getFile( ) ) );
+        XslExportHome.create( _xslExport );
+    }
+
+    @Override
+    protected void tearDown( ) throws Exception
+    {
+        XslExportHome.remove( _xslExport.getIdXslExport( ) );
+        FileHome.remove( _xslExport.getFile( ).getIdFile( ) );
+        super.tearDown( );
     }
 
     public void testGetCreateXslExport( ) throws AccessDeniedException
@@ -198,5 +227,111 @@ public class XslExportJspBeanTest extends LuteceTestCase
         Random rand = new SecureRandom( );
         BigInteger bigInt = new BigInteger( 128, rand );
         return "junit" + bigInt.toString( 36 );
+    }
+
+    public void testGetModifyXslExport( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        AdminUser user = new AdminUser( );
+        user.setRoles( AdminRoleHome.findAll( ).stream( )
+                .collect( Collectors.toMap( AdminRole::getKey, Function.identity( ) ) ) );
+        Utils.registerAdminUserWithRigth( request, user, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+        request.setParameter( "id_xsl_export", Integer.toString( _xslExport.getIdXslExport( ) ) );
+        _instance.init( request, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+        assertNotNull( _instance.getModifyXslExport( request ) );
+    }
+
+    public void testDoModifyXslExport( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        AdminUser user = new AdminUser( );
+        user.setRoles( AdminRoleHome.findAll( ).stream( )
+                .collect( Collectors.toMap( AdminRole::getKey, Function.identity( ) ) ) );
+        Utils.registerAdminUserWithRigth( request, user, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+        String randomName = getRandomName( );
+        Map<String, String[ ]> parameters = new HashMap<>( );
+        parameters.put( "title", new String[ ] { randomName } );
+        parameters.put( "description", new String[ ] { randomName } );
+        parameters.put( "extension", new String[ ] { randomName } );
+        parameters.put( "id_xsl_export", new String[ ] { Integer.toString( _xslExport.getIdXslExport( ) ) } );
+        parameters.put( SecurityTokenService.PARAMETER_TOKEN, new String[ ] {
+                SecurityTokenService.getInstance( ).getToken( request, "admin/xsl/modify_xsl_export.html" ) } );
+        Map<String, List<FileItem>> multipartFiles = new HashMap<>( );
+
+        _instance.init( request, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+
+        _instance.doModifyXslExport( new MultipartHttpServletRequest( request, multipartFiles, parameters ) );
+
+        XslExport stored = XslExportHome.findByPrimaryKey( _xslExport.getIdXslExport( ) );
+        assertNotNull( stored );
+        assertEquals( randomName, stored.getTitle( ) );
+        assertEquals( randomName, stored.getDescription( ) );
+        assertEquals( randomName, stored.getExtension( ) );
+    }
+
+    public void testDoModifyXslExportInvalidToken( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        AdminUser user = new AdminUser( );
+        user.setRoles( AdminRoleHome.findAll( ).stream( )
+                .collect( Collectors.toMap( AdminRole::getKey, Function.identity( ) ) ) );
+        Utils.registerAdminUserWithRigth( request, user, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+        String randomName = getRandomName( );
+        Map<String, String[ ]> parameters = new HashMap<>( );
+        parameters.put( "title", new String[ ] { randomName } );
+        parameters.put( "description", new String[ ] { randomName } );
+        parameters.put( "extension", new String[ ] { randomName } );
+        parameters.put( "id_xsl_export", new String[ ] { Integer.toString( _xslExport.getIdXslExport( ) ) } );
+        parameters.put( SecurityTokenService.PARAMETER_TOKEN, new String[ ] {
+                SecurityTokenService.getInstance( ).getToken( request, "admin/xsl/modify_xsl_export.html" ) + "b" } );
+        Map<String, List<FileItem>> multipartFiles = new HashMap<>( );
+
+        _instance.init( request, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+
+        try
+        {
+            _instance.doModifyXslExport( new MultipartHttpServletRequest( request, multipartFiles, parameters ) );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            XslExport stored = XslExportHome.findByPrimaryKey( _xslExport.getIdXslExport( ) );
+            assertNotNull( stored );
+            assertEquals( _xslExport.getTitle( ), stored.getTitle( ) );
+            assertEquals( _xslExport.getDescription( ), stored.getDescription( ) );
+            assertEquals( _xslExport.getExtension( ), stored.getExtension( ) );
+        }
+    }
+
+    public void testDoModifyXslExportNoToken( ) throws AccessDeniedException
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest( );
+        AdminUser user = new AdminUser( );
+        user.setRoles( AdminRoleHome.findAll( ).stream( )
+                .collect( Collectors.toMap( AdminRole::getKey, Function.identity( ) ) ) );
+        Utils.registerAdminUserWithRigth( request, user, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+        String randomName = getRandomName( );
+        Map<String, String[ ]> parameters = new HashMap<>( );
+        parameters.put( "title", new String[ ] { randomName } );
+        parameters.put( "description", new String[ ] { randomName } );
+        parameters.put( "extension", new String[ ] { randomName } );
+        parameters.put( "id_xsl_export", new String[ ] { Integer.toString( _xslExport.getIdXslExport( ) ) } );
+        Map<String, List<FileItem>> multipartFiles = new HashMap<>( );
+
+        _instance.init( request, XslExportJspBean.RIGHT_MANAGE_XSL_EXPORT );
+
+        try
+        {
+            _instance.doModifyXslExport( new MultipartHttpServletRequest( request, multipartFiles, parameters ) );
+            fail( "Should have thrown" );
+        }
+        catch ( AccessDeniedException e )
+        {
+            XslExport stored = XslExportHome.findByPrimaryKey( _xslExport.getIdXslExport( ) );
+            assertNotNull( stored );
+            assertEquals( _xslExport.getTitle( ), stored.getTitle( ) );
+            assertEquals( _xslExport.getDescription( ), stored.getDescription( ) );
+            assertEquals( _xslExport.getExtension( ), stored.getExtension( ) );
+        }
     }
 }
