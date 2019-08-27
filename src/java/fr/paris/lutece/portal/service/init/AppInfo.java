2a2893063ace1453d470984fd2a001a23ce4808f
ryahiaoui <ryahiaoui@users.noreply.github.com>
2019-08-27 11:32:52 +0200

    [site-release] Update AppInfo.java version

--- src/java/fr/paris/lutece/portal/service/init/AppInfo.java
+++ src/java/fr/paris/lutece/portal/service/init/AppInfo.java
@@ -39,7 +39,7 @@ package fr.paris.lutece.portal.service.init;
 public final class AppInfo
 {
     /** Defines the current version of the application */
-    private static final String APP_VERSION = "6.2.0";
+    private static final String APP_VERSION = "6.2.1-SNAPSHOT";
 
     /**
      * Creates a new AppInfo object.
