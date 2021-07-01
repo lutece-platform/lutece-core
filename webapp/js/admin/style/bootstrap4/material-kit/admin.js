302c5b9b4b7133bc9a4de2274c193eeb5044a01b
Elysa Jouve <elysa.jouve@paris.fr>
2021-07-01 11:54:39 +0200

    LUTECE-2341: Added code for collapse/expand button icons

--- webapp/js/admin/style/bootstrap4/material-kit/admin.js
+++ webapp/js/admin/style/bootstrap4/material-kit/admin.js
@@ -150,3 +150,13 @@ $( function(){
     });
 
 });
+
+// Toggle collapse buttons
+$('[data-toggle="collapse"]').click(function() {
+	if ($(this).find("i").hasClass("fa-minus")){
+    $(this).find("i").addClass("fa-plus").removeClass("fa-minus");
+    }
+	else if ($(this).find("i").hasClass("fa-plus")){
+    $(this).find("i").addClass("fa-minus").removeClass("fa-plus");
+    }
+});
