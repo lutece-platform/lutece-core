44e8d95db3eb078d93ce58c1b2c14421fd772ab0
HOHL <laurent.hohl@gmail.com>
2023-02-24 12:00:02 +0100

    Redmine-25017 : Add datastore keys for layout

--- src/sql/upgrade/update_db_lutece_core-7.0.7-7.1.0.sql
+++ src/sql/upgrade/update_db_lutece_core-7.0.7-7.1.0.sql
@@ -1,1 +1,4 @@
 ALTER TABLE core_feature_group ADD COLUMN feature_group_icon VARCHAR(255) DEFAULT NULL;
+
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.layout.class', 'sidebar');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.layout.code', 'header');
