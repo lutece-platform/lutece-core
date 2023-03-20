464274db702d977d758910b5dc2832b183cae716
HOHL <laurent.hohl@gmail.com>
2023-03-20 15:09:41 +0100

    LUT-25017 : Add missing datastore key

--- src/sql/upgrade/update_db_lutece_core-7.0.7-7.1.0.sql
+++ src/sql/upgrade/update_db_lutece_core-7.0.7-7.1.0.sql
@@ -1,6 +1,8 @@
 ALTER TABLE core_feature_group ADD COLUMN feature_group_icon VARCHAR(255) DEFAULT NULL;
 INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.layout.class', 'sidebar');
 INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.layout.code', 'header');
+DELETE FROM `core_datastore` WHERE `entity_key`='portal.site.site_property.layout.login';
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.layout.login', 'cover');
 DELETE FROM `core_datastore` WHERE `entity_key` = 'portal.site.site_property.logo_url';
 INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('portal.site.site_property.logo_url', 'themes/admin/shared/images/logo-header-icon.png');
 DELETE FROM `core_datastore` WHERE `entity_key` = 'portal.site.site_property.layout.login.image';
