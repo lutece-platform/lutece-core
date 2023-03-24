b4f275afd63ab9bee2e61b3571a17ec501198556
Jean-Baptiste V <jbvanhems@gmail.com>
2023-03-24 11:42:07 +0100

    LUT-25170 : update system templates

--- webapp/js/modules/luteceContentLoader.js
+++ webapp/js/modules/luteceContentLoader.js
@@ -2,53 +2,111 @@
  * Loads remote HTML content into a target element.
  */
 export default class LuteceContentLoader extends EventTarget {
-    /**
-     * Creates a new instance of `LuteceContentLoader`.
-     *
-     * @param {string} url - The URL of the remote HTML content.
-     * @param {string} element - The selector for the target element in the HTML content.
-     */
-    constructor(url, element) {
-      super();
-      this.url = url;
-      this.element = element;
-      this.controller = new AbortController();
-    }
-  
-    /**
-     * Loads the remote HTML content and emits appropriate events.
-     *
-     * @fires LuteceContentLoader#start
-     * @fires LuteceContentLoader#success
-     * @fires LuteceContentLoader#warning
-     * @fires LuteceContentLoader#error
-     */
-    async load() {
-      try {
-        this.dispatchEvent(new Event('start'));
-        const response = await fetch(this.url, { signal: this.controller.signal });
-        const html = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const targetElement = doc.querySelector(this.element);
-  
-        if (targetElement) {
-          this.dispatchEvent(new CustomEvent('success', { detail: targetElement }));
-        } else {
-          this.dispatchEvent(new CustomEvent('warning', { detail: doc }));
-        }
-      } catch (error) {
-        if (error.name !== 'AbortError') {
-          this.dispatchEvent(new CustomEvent('error', { detail: error }));
-        }
-      }
-    }
-  
-    /**
-     * Cancel the current loading process if one is in progress.
-     */
-    cancel() {
+  /**
+   * Creates a new instance of `LuteceContentLoader`.
+   *
+   * @param {string} targetUrl - The URL of the remote HTML content.
+   * @param {string} targetElement - The selector for the target element in the HTML content.
+   */
+  constructor(targetUrl, targetElement) {
+    super();
+    this.targetUrl = targetUrl;
+    this.targetElement = targetElement;
+    this.dataStore = {}; 
+    this.controller = new AbortController();
+  }
+
+  /**
+   * Loads the remote HTML content and emits appropriate events.
+   *
+   * @fires LuteceContentLoader#start
+   * @fires LuteceContentLoader#success
+   * @fires LuteceContentLoader#warning
+   * @fires LuteceContentLoader#error
+   */
+  async load() {
+    
+    if (this.controller) {
       this.controller.abort();
     }
+    this.controller = new AbortController();
+
+    try {
+      this.dispatchEvent(new Event('start'));
+      const response = await fetch(this.targetUrl, {
+        signal: this.controller.signal
+      });
+      const text = await response.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(text, 'text/html');
+      const targetElement = doc.querySelector(this.targetElement);
+
+      if (targetElement) {
+        this.dispatchEvent(new CustomEvent('success', {
+          detail: {
+            dataStore : this.dataStore,
+            targetElement : targetElement
+          }
+        }));
+      } else {
+        this.dispatchEvent(new CustomEvent('warning', {
+          detail: {
+            dataStore : this.dataStore,
+            document: doc
+          }
+        }));
+      }
+    } catch (error) {
+      if (error.name !== 'AbortError') {
+        this.dispatchEvent(new CustomEvent('error', {
+          detail: {
+            dataStore : this.dataStore,
+            error: error
+          }
+        }));
+      }
+    }
   }
-  
+  /**
+   * Cancel the current loading process if one is in progress.
+   */
+  cancel() {
+    this.controller.abort();
+  }
+
+  /**
+   * Set url of the remote HTML content.
+   * @param {string} targetUrl 
+   */
+  setTargetUrl(targetUrl) {
+    this.targetUrl = targetUrl;
+  }
+
+  /**
+   * Set selector for the target element in the HTML content.
+   * @param {string} element 
+   */
+  setTargetElement(targetElement) {
+    this.targetElement = targetElement;
+  }
+
+   /**
+   * Stores custom data in the instance.
+   *
+   * @param {string} key - The key under which the data will be stored.
+   * @param {*} value - The value to be stored.
+   */
+   setDataStoreItem(key, value) {
+    this.dataStore[key] = value;
+  }
+
+  /**
+   * Retrieves custom data from the instance.
+   *
+   * @param {string} key - The key under which the data is stored.
+   * @returns {*} The stored data, or undefined if the key does not exist.
+   */
+  getDataStoreItem(key) {
+    return this.dataStore[key];
+  }
+}
--- webapp/js/modules/luteceSearchList.js
+++ webapp/js/modules/luteceSearchList.js
@@ -2,124 +2,113 @@
 A class for searching through a list of elements and highlighting search matches.
 */
 export default class LuteceSearchList {
-    /**
-     * Creates a new LuteceList instance.
-     * @param {HTMLInputElement} searchInput - The search input element.
-     * @param {HTMLElement[]} elementsList - The list of elements to search through.
-     * @param {Object} options - The options object.
-     * @param {boolean} options.highlight - Whether to highlight search matches.
-     */
-    constructor(
-      searchInput,
-      elementsList,
-      options = {
-        highlight: true,
-      }
-    ) {
-      this.searchInput = searchInput;
-      this.elementsList = elementsList;
-      this.highlight = options.highlight;
-      // remove animation and transition to avoid flickering
-      this.elementsList.forEach((element) => {
-        element.style.transition = "none";
-        element.style.animation = "none";
-        Array.from(element.childNodes).forEach((child) => {
-          if (child.nodeType === 1) {
-            // only target element nodes
-            child.style.transition = "none";
-            child.style.animation = "none";
-          }
-        });
-      });
-      this.init();
-    }
-    /**
-     * Initializes the search input event listener.
-     */
-    init() {
-      this.searchInput.addEventListener(
-        "input",
-        this.debounce((event) => {
-          this.search(event.target.value);
-        }, 300)
-      );
-      this.searchInput.addEventListener(
-        "keydown",
-        (event) => {
-          if (event.keyCode === 13) {
-            event.preventDefault();
-          }
-        });
-    }
-    /**
-     * Debounces a function by delaying its execution until the specified time has passed since the last invocation.
-     * @param {Function} func - The function to debounce.
-     * @param {number} wait - The delay in milliseconds.
-     * @returns {Function} The debounced function.
-     */
-    debounce(func, wait) {
-      let timeout;
-      return function (...args) {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), wait);
-      };
-    }
-    /**
-     * Searches the elements list for the query and updates the display accordingly.
-     * @param {string} query - The search query.
-     */
-    search() {
-      const searchTerm = this.searchInput.value.trim().toLowerCase();
-      this.elementsList.forEach((element) => {
-        this.removeHighlight(element);
-        if (searchTerm !== "") {
-          const text = element.textContent.toLowerCase();
-          if (text.includes(searchTerm)) {
-            this.highlight && this.addHighlight(element, searchTerm);
-            this.showElement(element);
+  /**
+   * Creates a new instance of LuteceSearchList.
+   * @param {HTMLElement} searchInput - The search input element.
+   * @param {NodeList} searchElementList - The list of elements to search through.
+   * @param {Object} [options] - Optional parameters for customizing search behavior.
+   */
+  constructor(searchInput, searchElementList, options) {
+    this.searchInput = searchInput;
+    this.searchElementList = searchElementList;
+    this.options = Object.assign({
+      searchableChild: [],
+      highlight: false,
+      debounceTime: 100,
+    }, options);
+    this.init();
+  }
+  /**
+   * Initializes the search functionality by adding an event listener to the search input element.
+   */
+  init() {
+    this.searchInput.addEventListener('input', this.debounce(() => {
+      const searchTerm = this.searchInput.value.toLowerCase();
+      this.searchElementList.forEach(element => {
+        const searchContent = this.getSearchContent(element);
+        const isVisible = searchContent.includes(searchTerm);
+        if (this.options.highlight) {
+          if (searchTerm.length > 0) {
+            this.removeHighlight(element);
+            this.addHighlight(element, searchTerm);
           } else {
-            this.hideElement(element);
+            this.removeHighlight(element);
           }
-        } else {
-          this.showElement(element);
+        }
+        element.style.display = isVisible ? '' : 'none';
+      });
+    }, this.options.debounceTime));
+    this.searchInput.addEventListener(
+      "keydown",
+      (event) => {
+        if (event.keyCode === 13) {
+          event.preventDefault();
         }
       });
-    }
-    /**
-     * Highlights the search matches in an element while maintaining the original formatting.
-     * @param {HTMLElement} element - The element to highlight.
-     * @param {RegExp} regex - The regular expression to match the search query.
-     */
-    addHighlight(element, searchTerm) {
-      const regex = new RegExp(`(>[^<]*?)(${searchTerm})([^>]*?<)`, "gi");
-      const originalInnerHTML = element.innerHTML;
-      const highlightedInnerHTML = originalInnerHTML.replace(
-        regex,
-        "$1<mark>$2</mark>$3"
-      );
-      element.innerHTML = highlightedInnerHTML;
-    }
-    /**
-     * Remove hightlight from an element.
-     * @param {HTMLElement} element - The element to reset.
-     */
-    removeHighlight(element) {
-      const markRegex = /(<mark>|<\/mark>)/gi;
-      element.innerHTML = element.innerHTML.replace(markRegex, "");
-    }
-    /**
-     * Shows an element.
-     * @param {HTMLElement} element - The element to show.
-     */
-    showElement(element) {
-      element.style.display = "";
-    }
-    /**
-     * hide an element.
-     * @param {HTMLElement} element - The element to hide.
-     */
-    hideElement(element) {
-      element.style.display = "none";
-    }
   }
+  /**
+   * Returns the searchable content of an element as a string.
+   * @param {HTMLElement} element - The element to get the searchable content for.
+   * @returns {string} The searchable content of the element.
+   */
+  getSearchContent(element) {
+    if (this.options.searchableChild.length === 0) {
+      return element.textContent.toLowerCase();
+    }
+    return this.options.searchableChild
+      .map(selector => {
+        const children = element.querySelectorAll(selector);
+        return Array.from(children)
+          .map(child => child.textContent.toLowerCase())
+          .join(' ');
+      })
+      .join(' ');
+  }
+  /**
+   * Returns a debounced version of a function.
+   * @param {Function} func - The function to debounce.
+   * @param {number} wait - The time in milliseconds to wait before executing the debounced function.
+   * @returns {Function} The debounced function.
+   */
+  debounce(func, wait) {
+    let timeout;
+    return (...args) => {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+  }
+  /**
+   * Highlights search matches in an element's children.
+   * @param {HTMLElement} element - The element to highlight search matches in.
+   * @param {string} searchTerm - The search term to highlight.
+   */
+  addHighlight(element, searchTerm) {
+    this.options.searchableChild.forEach(selector => {
+      const children = element.querySelectorAll(selector);
+      children.forEach(child => {
+        const regex = new RegExp(`(>[^<]*?)(${searchTerm})([^>]*?<)`, "gi");
+        const originalInnerHTML = child.innerHTML;
+        const highlightedInnerHTML = originalInnerHTML.replace(
+          regex,
+          "$1<mark>$2</mark>$3"
+        );
+        child.innerHTML = highlightedInnerHTML;
+      });
+    });
+  }
+  /**
+   * Removes highlighting from an element's children.
+   * @private
+   * @param {HTMLElement} element - The element to remove highlighting from.
+   */
+  removeHighlight(element) {
+    this.options.searchableChild.forEach(selector => {
+      const children = element.querySelectorAll(selector);
+      children.forEach(child => {
+        const regex = /(<mark>|<\/mark>)/gi;
+        child.innerHTML = child.innerHTML.replace(regex, '');
+      });
+    });
+  }
+}
