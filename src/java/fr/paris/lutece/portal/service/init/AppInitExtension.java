6709298d00e9d3603c06850109539ca87e42eb1d
yahiaoui <rafik.yahiaoui@paris.fr>
2024-03-01 11:25:25 +0100

    LUT-27493: Remove the log4j-web dependency is necessary as it no longer meets the requirement to initialize log4j at the startup of the Lutece web application

---
+++ src/java/fr/paris/lutece/portal/service/init/AppInitExtension.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2002-2023, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.portal.service.init;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.service.util.AppPathService;
+import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
+import jakarta.enterprise.inject.spi.Extension;
+
+/**
+ * The initialization service of the application.
+ */
+public class AppInitExtension implements Extension
+{
+    private static final String PATH_CONF = "/WEB-INF/conf/";
+
+	
+    protected void initPropertiesServices( @Observes final BeforeBeanDiscovery bd )
+    {    	
+    	if(Files.notExists(Paths.get(AppPathService.getWebAppPath( )+PATH_CONF)))
+    	{
+    		String _strResourcesDir = getClass( ).getResource( "/" ).toString( ).replaceFirst( "file:", "" ).replaceFirst( "classes", "lutece" );
+             AppPathService.init( _strResourcesDir );
+    	}
+    	System.setProperty("log4j.configurationFile", "file:"+AppPathService.getWebAppPath()+"/WEB-INF/conf/log.properties" );
+    	AppInit.initPropertiesServices( PATH_CONF, AppPathService.getWebAppPath( ) );
+    }
+
+    
+}
