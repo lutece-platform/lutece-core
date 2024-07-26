ea393f5445d2389699909c429ad09a04a6281b1d
obfromevi <obfromevi@gmail.com>
2024-07-26 15:51:16 +0200

    LUT-28182 : add a confirmation as a notification when a manual workflow action is triggered and correctly processed

--- src/java/fr/paris/lutece/portal/service/workflow/WorkflowService.java
+++ src/java/fr/paris/lutece/portal/service/workflow/WorkflowService.java
@@ -33,6 +33,7 @@
  */
 package fr.paris.lutece.portal.service.workflow;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
@@ -47,6 +48,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 import fr.paris.lutece.api.user.User;
 import fr.paris.lutece.plugins.workflowcore.business.action.Action;
+import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
 import fr.paris.lutece.plugins.workflowcore.business.state.State;
 import fr.paris.lutece.plugins.workflowcore.service.workflow.IWorkflowService;
 import fr.paris.lutece.portal.business.event.ResourceEvent;
@@ -276,17 +278,18 @@ public final class WorkflowService
      * @param user
      *            The User
      */
-    public void doProcessAction( int nIdResource, String strResourceType, int nIdAction, Integer nExternalParentId, HttpServletRequest request, Locale locale,
+    public List<ResourceHistory> doProcessAction( int nIdResource, String strResourceType, int nIdAction, Integer nExternalParentId, HttpServletRequest request, Locale locale,
             boolean bIsAutomatic, User user )
     {
         if ( isAvailable( ) && canProcessAction( nIdResource, strResourceType, nIdAction, nExternalParentId, request, bIsAutomatic, user ) )
         {
             TransactionManager.beginTransaction( null );
-
+            List<ResourceHistory> actionHistoryResourceList = new ArrayList<>( );
+            
             try
             {
                 String strUserAccessCode = bIsAutomatic ? null : _provider.getUserAccessCode( request, user );
-                _service.doProcessAction( nIdResource, strResourceType, nIdAction, nExternalParentId, request, locale, bIsAutomatic, strUserAccessCode, user );
+                _service.doProcessAction( nIdResource, strResourceType, nIdAction, nExternalParentId, request, locale, bIsAutomatic, strUserAccessCode, user, actionHistoryResourceList );
                 TransactionManager.commitTransaction( null );
 
                 registerResourceEvent( nIdResource, strResourceType );
@@ -296,7 +299,11 @@ public final class WorkflowService
                 TransactionManager.rollBack( null );
                 throw new AppException( e.getMessage( ), e );
             }
+            
+            return actionHistoryResourceList;
         }
+        
+        return null;
     }
 
     /**
@@ -453,8 +460,37 @@ public final class WorkflowService
      *            the user
      * @return null if there is no error in the task form else return the error message url
      */
+    @Deprecated
     public String doSaveTasksForm( int nIdResource, String strResourceType, int nIdAction, Integer nExternalParentId, HttpServletRequest request, Locale locale,
             User user )
+    {
+    	return doSaveTasksForm( nIdResource, strResourceType, nIdAction, nExternalParentId, request, locale, null, null );
+    }
+
+    /**
+     * Perform the information on the various tasks associated with the given action specified in parameter
+     * 
+     * @param nIdResource
+     *            the resource id
+     * @param strResourceType
+     *            the resource type
+     * @param nExternalParentId
+     *            the external parent id
+     * @param request
+     *            the request
+     * @param nIdAction
+     *            the action id
+     * @param locale
+     *            the locale
+     * @param user
+     *            the user
+     * @param actionHistoryResourceList
+     *            the history of action performed on a resource
+     * 
+     * @return null if there is no error in the task form else return the error message url
+     */
+    public String doSaveTasksForm( int nIdResource, String strResourceType, int nIdAction, Integer nExternalParentId, HttpServletRequest request, Locale locale,
+            User user, List<ResourceHistory> actionHistoryResourceList )
     {
         if ( isAvailable( ) )
         {
@@ -465,7 +501,12 @@ public final class WorkflowService
                 return strError;
             }
 
-            doProcessAction( nIdResource, strResourceType, nIdAction, nExternalParentId, request, locale, false, user );
+            List<ResourceHistory> actionHistoryList = doProcessAction( nIdResource, strResourceType, nIdAction, nExternalParentId, request, locale, false, user );
+            
+            if ( actionHistoryResourceList != null && actionHistoryList != null )
+            {
+            	actionHistoryResourceList.addAll( actionHistoryList );
+            }
         }
 
         return null;
