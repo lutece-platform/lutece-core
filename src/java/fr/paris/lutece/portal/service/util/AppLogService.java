45840e9b101f955eff4471bbc38c3b5f13a06589
yahiaoui <rafik.yahiaoui@paris.fr>
2024-11-22 16:31:47 +0100

    LUT-29025:fix compilation bug

--- src/java/fr/paris/lutece/portal/service/util/AppLogService.java
+++ src/java/fr/paris/lutece/portal/service/util/AppLogService.java
@@ -33,13 +33,10 @@
  */
 package fr.paris.lutece.portal.service.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.util.Supplier;
 
 /**
