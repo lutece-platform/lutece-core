407c19faaab78e69c7d64860f1b9d13913cf4b93
Anas BENJELLOUN <anasbenjellountouimi@gmail.com>
2025-04-02 11:54:01 +0200

    LUT-29784 error in script update_db_lutece_core-7.10.-7.1.1.sql: missing a ; on line 8

--- src/sql/upgrade/update_db_lutece_core-7.1.0-7.1.1.sql
+++ src/sql/upgrade/update_db_lutece_core-7.1.0-7.1.1.sql
@@ -5,7 +5,7 @@ INSERT INTO core_text_editor VALUES ('tinymce6', 'portal.admindashboard.editors.
 UPDATE core_datastore SET entity_value='tinymce6' where entity_key ='core.backOffice.defaultEditor' and entity_value = 'tinymce';
 DELETE FROM core_text_editor WHERE editor_name='tinymce';
 UPDATE core_datastore SET entity_key='portal.site.site_property.layout.login.image' WHERE entity_key='portal.site.site_property.login.image';
-DELETE FROM core_datastore WHERE entity_key='portal.site.site_property.login.image'
+DELETE FROM core_datastore WHERE entity_key='portal.site.site_property.login.image';
 
 --
 -- Remove log page
