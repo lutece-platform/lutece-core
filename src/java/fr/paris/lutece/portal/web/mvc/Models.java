b4f1809532e03432ca74637bb76012632d0bb083
yahiaoui <rafik.yahiaoui@paris.fr>
2025-04-07 11:48:30 +0200

    LUT-29811: Integrating Models for Enhanced Model Injection in Lutece

---
+++ src/java/fr/paris/lutece/portal/web/mvc/Models.java
@@ -0,0 +1,52 @@
+package fr.paris.lutece.portal.web.mvc;
+
+import java.util.Map;
+
+/**
+ * <p>A map of name to model instances used by a to process a view. Instances implementing this interface must be injectable using
+ * {@link jakarta.inject.Inject} and are {@link jakarta.enterprise.context.RequestScoped}.
+ *
+ * <p>Note that certain view engines, such as the engine for Jakarta Server Pages, support
+ * model binding via {@link jakarta.inject.Named} in which case the use of Models is
+ * optional.
+ *
+ * @see jakarta.inject.Named
+ * @see jakarta.enterprise.context.RequestScoped
+ * @since 8.0.0
+ */
+public interface Models extends Iterable<String> {
+
+	 /**
+     * Stores a new model in the map.
+     *
+     * @param name  name of the model
+     * @param model model to store in the map
+     * @return the current instance to allow method chaining
+     */
+    Models put(String name, Object model);
+
+    /**
+     * Retrieve a model by name.
+     *
+     * @param name name of the model
+     * @return the model or <code>null</code>
+     */
+    Object get(String name);
+
+    /**
+     * Retrieve a model by name in a type-safe way.
+     *
+     * @param name  name of the model
+     * @param clazz type of the model
+     * @param <T>   type of the model
+     * @return The model or <code>null</code>
+     */
+    <T> T get(String name, Class<T> clazz);
+
+    /**
+     * Returns a unmodifiable view of the models map.
+     *
+     * @return unmodifiable map
+     */
+    Map<String, Object> asMap();
+}
---
+++ src/java/fr/paris/lutece/portal/web/mvc/ModelsImpl.java
@@ -0,0 +1,49 @@
+package fr.paris.lutece.portal.web.mvc;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ * Implementation of {@link fr.paris.lutece.portal.web.mvc.Models} interface. A CDI class that delegates
+ * to a {@link java.util.Map} implementation.
+ *
+ */
+@RequestScoped
+public class ModelsImpl implements Models {
+
+	 private final Map<String, Object> map = new LinkedHashMap<>();
+
+	    @Override
+	    public Models put(String name, Object model) {
+	        Objects.requireNonNull(name, "Name must not be null");
+	        map.put(name, model);
+	        return this;
+	    }
+
+	    @Override
+	    public Object get(String name) {
+	        return get(name, Object.class);
+	    }
+
+	    @Override
+	    public <T> T get(String name, Class<T> type) {
+	        Objects.requireNonNull(name, "Name must not be null");
+	        Objects.requireNonNull(type, "Type must not be null");
+	        return type.cast(map.get(name));
+	    }
+
+	    @Override
+	    public Map<String, Object> asMap() {
+	        return Collections.unmodifiableMap(new LinkedHashMap<>(map));
+	    }
+
+	    @Override
+	    public Iterator<String> iterator() {
+	        return map.keySet().iterator();
+	    }
+}
