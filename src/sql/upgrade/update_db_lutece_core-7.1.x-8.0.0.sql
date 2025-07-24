84388a812d03ed177a59ba83b3c714eddca8ac6b
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-07-24 15:42:03 +0200

    LUT-30417 : Prepare SQL scripts for Liquibase plugin

--- src/sql/upgrade/update_db_lutece_core-7.1.x-8.0.0.sql
+++ src/sql/upgrade/update_db_lutece_core-7.1.x-8.0.0.sql
@@ -1,2 +1,5 @@
+--liquibase formatted sql
+--changeset core:update_db_lutece_core-7.1.x-8.0.0.sql
+--preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE core_level_right ALTER COLUMN id_level DROP DEFAULT;
 DROP TABLE IF EXISTS core_mode;
