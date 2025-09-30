834b30f54d80c286844f91fc92a60adfa158c1d4
Jeremy Deloche <jeremy.deloche@atos.com>
2025-09-30 15:43:21 +0200

    LUT-30836 : SQL scripts for Liquibase update

--- src/sql/upgrade/update_db_lutece_core-7.0.6-7.0.7.sql
+++ src/sql/upgrade/update_db_lutece_core-7.0.6-7.0.7.sql
@@ -1,4 +1,4 @@
---liquibase formatted sql
---changeset core:update_db_lutece_core-7.0.6-7.0.7.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset core:update_db_lutece_core-7.0.6-7.0.7.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE core_file ADD COLUMN origin VARCHAR(255) DEFAULT NULL;
