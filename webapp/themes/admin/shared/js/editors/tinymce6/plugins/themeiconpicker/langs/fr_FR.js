c3998d5365e34dcc5ceacdb6c836a67f596c647b
hohll <hohll@users.noreply.github.com>
2026-06-18 09:02:26 +0200

    LUT-32805 : fIx missing themeiconpicker.js plugin for tinyMce and fix default css resources path in tinyMce init

---
+++ webapp/themes/admin/shared/js/editors/tinymce6/plugins/themeiconpicker/langs/fr_FR.js
@@ -0,0 +1,8 @@
+tinymce.addI18n( 'fr_FR', {
+  'Select Icon': 'Sélectionner une icône',
+  'Search icons': 'Rechercher des icônes',
+  'Type to search...': 'Tapez pour rechercher...',
+  'Insert Icon': 'Insérer une icône',
+  'Cancel': 'Fermer',
+});
+  
