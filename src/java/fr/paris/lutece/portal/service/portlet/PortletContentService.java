1adc2a3a00fe128ab077ec950df3d8cb13dc3b28
rayane kaabeche <rayanoo1302@gmail.com>
2026-08-10 11:28:31 +0200

    LUT-33136 : adding displayPortletTitle to alias portlet

--- src/java/fr/paris/lutece/portal/service/portlet/PortletContentService.java
+++ src/java/fr/paris/lutece/portal/service/portlet/PortletContentService.java
@@ -144,6 +144,10 @@ public class PortletContentService
             strPortletContent = ADMIN_PORTLET_OPEN_TAG + addAdminButtons( request, portlet );
         }
 
+        if(portlet.getDisplayPortletTitle() == 1){
+            portlet.setName(null);
+        }
+
         String strKey = StringUtils.EMPTY;
 
         LuteceUser user = null;
