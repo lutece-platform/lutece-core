2cfa7e73d39aa8877abb863c5f7e9a0b8271dd0b
hohll <hohll@users.noreply.github.com>
2026-09-23 09:06:52 +0200

    LUT-33483 : Remove Jquery from progress-manager.js in BO and FO

--- webapp/themes/skin/shared/js/util/progress-manager.js
+++ webapp/themes/skin/shared/js/util/progress-manager.js
@@ -1,110 +1,138 @@
+/* Lutece progress manager - vanilla JS (no jQuery dependency) */
 
-function getProgress( id, feedToken, intervalTime ) {
-    $.ajax({
-        url: 'servlet/plugins/core/progressManager/progressFeed?progress&token=' + feedToken ,
-        method: 'POST',
-        async: true,
-        success: function(data) {
-        	if (data.status != "ERROR") {
-			updateProgressBar( id, parseInt(data.result) );
-		        if ( getProgressValue( id ) < getProgressMaxValue( id ) ) {
-		            setTimeout(  function () { getProgress( id, feedToken, intervalTime ) } , intervalTime ) ; 
-		        } 
-		} else {
-			updateProgressReport( id, { "lines" : [ data.status, data.errorCode ], "lastLine" : 0 } , true );
-		}
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-                updateProgressReport( id, { "lines" : [ textStatus, errorThrown ], "lastLine" : 0 } , true );
-        }
-    });
+const PROGRESS_FEED_URL = 'servlet/plugins/core/progressManager/progressFeed';
+
+function progressElement(id) {
+    return document.getElementById(id);
 }
 
-function getReport( id, feedToken, nfromLine, intervalTime ) {
-    $.ajax({
-        url: 'servlet/plugins/core/progressManager/progressFeed?report&fromLine=' + nfromLine + '&token=' + feedToken ,
-        method: 'POST',
-        async: true,
-        success: function(data) {
-        	if (data.status != "ERROR") {
-		        updateProgressReport( id, data.result, true );
-		        if ( getProgressValue( id ) < getProgressMaxValue( id ) ) {
-		            setTimeout(  function () { getReport( id, feedToken, parseInt( $('#'+id+"-report").attr( "lastline" ) ), intervalTime ) } , intervalTime );
-		        } 
-		} else {
-			updateProgressReport( id, { "lines" : [ data.status, data.errorCode ], "lastLine" : 0 } , true );
-		}
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-                updateProgressReport( id, { "lines" : [ textStatus, errorThrown ], "lastLine" : 0 } , true );
-        }
-    });
+function reportElement(id) {
+    return document.getElementById(id + '-report');
 }
- 
-function updateProgressBar(id, percentage){
-    if ( $('#'+id).prop("tagName") == "PROGRESS" ) {
-        $('#'+id).attr('value', percentage);
-    } else {
-        $('#'+id).attr('aria-valuenow', percentage);
-        $('#'+id).attr('style', 'width: ' + percentage + '%;');
+
+function isProgressTag(element) {
+    return element !== null && element.tagName === 'PROGRESS';
+}
+
+function fetchFeed(query) {
+    return fetch(PROGRESS_FEED_URL + '?' + query, { method: 'POST' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status + ' ' + response.statusText);
+            }
+            return response.json();
+        });
+}
+
+function reportError(id, lines) {
+    updateProgressReport(id, { lines: lines, lastLine: 0 }, true);
+}
+
+function getProgress(id, feedToken, intervalTime) {
+    fetchFeed('progress&token=' + encodeURIComponent(feedToken))
+        .then(data => {
+            if (data.status !== 'ERROR') {
+                updateProgressBar(id, parseInt(data.result, 10));
+                if (getProgressValue(id) < getProgressMaxValue(id)) {
+                    setTimeout(() => getProgress(id, feedToken, intervalTime), intervalTime);
+                }
+            } else {
+                reportError(id, [data.status, data.errorCode]);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            reportError(id, [error.message]);
+        });
+}
+
+function getReport(id, feedToken, nfromLine, intervalTime) {
+    fetchFeed('report&fromLine=' + nfromLine + '&token=' + encodeURIComponent(feedToken))
+        .then(data => {
+            if (data.status !== 'ERROR') {
+                updateProgressReport(id, data.result, true);
+                if (getProgressValue(id) < getProgressMaxValue(id)) {
+                    setTimeout(() => {
+                        const report = reportElement(id);
+                        const lastLine = report ? parseInt(report.getAttribute('lastline'), 10) || 0 : 0;
+                        getReport(id, feedToken, lastLine, intervalTime);
+                    }, intervalTime);
+                }
+            } else {
+                reportError(id, [data.status, data.errorCode]);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            reportError(id, [error.message]);
+        });
+}
+
+function updateProgressBar(id, percentage) {
+    const element = progressElement(id);
+    if (element === null) {
+        return;
     }
-        
-    $('#'+id).html( percentage+' /'+ getProgressMaxValue( id ) );
+    if (isProgressTag(element)) {
+        element.value = percentage;
+    } else {
+        element.setAttribute('aria-valuenow', percentage);
+        element.style.width = percentage + '%';
+    }
+    element.textContent = percentage + ' /' + getProgressMaxValue(id);
 }
 
 function updateProgressReport(id, report, isAppend) {
-    if ( !isAppend ) {
-        $('#'+id+"-report").html( report.lines.join("<br />") );
+    const element = reportElement(id);
+    if (element === null || !report || !Array.isArray(report.lines)) {
+        return;
+    }
+    if (!isAppend) {
+        element.innerHTML = report.lines.join('<br />');
     } else {
-    $('#'+id+"-report").append( new Date().toLocaleTimeString( document.documentElement.lang ) + "<br />");
-        
-        $('#'+id+"-report").append( report.lines.join("<br />") +"<br />" );
-        $('#'+id+"-report").attr( "lastline" , report.lastLine );
+        element.insertAdjacentHTML('beforeend', new Date().toLocaleTimeString(document.documentElement.lang) + '<br />');
+        element.insertAdjacentHTML('beforeend', report.lines.join('<br />') + '<br />');
+        element.setAttribute('lastline', report.lastLine);
     }
 }
 
-function getProgressMaxValue( id ){
-    if ( $('#'+id).prop("tagName") == "PROGRESS" )
-    {
-	return parseInt($('#'+id).attr('max'));
+function getProgressMaxValue(id) {
+    const element = progressElement(id);
+    if (element === null) {
+        return 0;
     }
-    else
-    {
-        return parseInt($('#'+id).attr('aria-valuemax'));
-    }
+    return parseInt(isProgressTag(element) ? element.getAttribute('max') : element.getAttribute('aria-valuemax'), 10);
 }
 
-function getProgressValue( id ){
-    if ( $('#'+id).prop("tagName") == "PROGRESS" )
-    {
-	return parseInt($('#'+id).attr('value'));
+function getProgressValue(id) {
+    const element = progressElement(id);
+    if (element === null) {
+        return 0;
     }
-    else
-    {
-        return parseInt($('#'+id).attr('aria-valuenow'));
-    }
+    return parseInt(isProgressTag(element) ? element.value : element.getAttribute('aria-valuenow'), 10);
 }
 
-function processError( id, errorMsg ) {
-    updateProgressReport( id, errorMsg, true );
+function processError(id, errorMsg) {
+    updateProgressReport(id, errorMsg, true);
 }
 
+function initProgressManagers() {
+    document.querySelectorAll('.progressmanager').forEach(element => {
+        const id = element.getAttribute('id');
+        const token = element.getAttribute('token');
+        const showReport = element.getAttribute('showReport') === 'true';
+        const intervalTime = parseInt(element.getAttribute('intervalTime'), 10) || 2000;
 
-$(document).ready( function( ) {
-    $('.progressmanager').each( function() {
-            var id = $( this ).attr( "id" );
-            var token = $( this ).attr( "token" );
-            var showReport = $( this ).attr( "showReport" );
-            var intervalTime = $( this ).attr( "intervalTime" );
-            
-            // get progress
-            getProgress( id, token, intervalTime );
-            
-            if ( showReport ) {
-                getReport( id, token, 0, intervalTime );
-            }
+        getProgress(id, token, intervalTime);
+
+        if (showReport) {
+            getReport(id, token, 0, intervalTime);
+        }
     });
-        
-});    
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initProgressManagers);
+} else {
+    initProgressManagers();
+}
